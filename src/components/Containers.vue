3951534a80f429e038865ce1ba7badc48b36de3a
Marcin van de Ven <marcinvandeven@gmail.com>
2020-02-17 15:57:04 +0100

    Added first idea
    First idea of implementation. Using collapse-elements.

--- src/components/Containers.vue
+++ src/components/Containers.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    Hierin komt een container manager
+  <div class="container-box">
+    <h4>Containers</h4>
+      <div class="container-head" v-b-toggle.container-1>Container 1</div>
+        <b-collapse id="container-1" class="mt-1">
+          <p>Some content</p>
+        </b-collapse>
+    <div div class="container-head" v-b-toggle.container-2>Container 2</div>
+    <b-collapse id="container-2" class="mt-1">
+      <p>Some other content</p>
+    </b-collapse>
   </div>
 </template>
 
@@ -11,5 +19,14 @@ export default {
 </script>
 
 <style scoped>
-
+  .container-box {
+    border: 1px;
+    border-style: solid;
+    border-color: #DD2400;
+    width: 350px;
+  }
+  .container-head {
+    margin-bottom: 10px;
+    background-color: silver;
+  }
 </style>
--- src/views/PointOfSale.vue
+++ src/views/PointOfSale.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="pointOfSale">
       <h1>Point of sale page</h1>
+      <Containers></Containers>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Containers from '@/components/Containers.vue';
 
 @Component({
-  components: {},
+  components: {
+    Containers,
+  },
 })
 
 export default class PointOfSale extends Vue {
