999b6bcda85685c5fc05a5f42b9b3504e707a85e
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2020-02-17 17:23:55 +0100

    Improved css formatting of table

---
+++ src/components/AdvertisementsList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th>Thumbnail</th>
+        <th>Duur (ms)</th>
+        <th>Actief</th>
+        <th>Toegevoegd op</th>
+        <th style="width: 0"></th>
+        <th style="width: 0"></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-bind:key="ad.id" v-for="ad in advertisements">
+        <td><img class="thumbnail" :src="ad.thumbnail" alt=""></td>
+        <td>{{ ad.duration }}</td>
+        <td>
+          <font-awesome-icon icon="check-circle"/>
+        </td>
+        <td>{{ ad.added.toLocaleString() }}</td>
+        <td><a href="https://gewis.nl/">
+          <font-awesome-icon icon="pencil-alt" class="font-awesome-icon"/>
+        </a></td>
+        <td><a href="https://gewis.nl/">
+          <font-awesome-icon icon="times" class="font-awesome-icon"/>
+        </a></td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Advertisements',
+  props: ['advertisements'],
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../styles/global/_table.scss';
+
+  .thumbnail {
+    width: 64px;
+    height: 36px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-  faCoffee, faChild, faCircle, faArchive,
+  faCoffee, faChild, faCircle, faArchive, faCheckCircle, faPencilAlt, faTimes,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
@@ -13,7 +13,7 @@ import 'bootstrap';
 // Import the BootstrapVue style
 import './styles/global/main.scss';
 
-library.add(faCoffee, faChild, faCircle, faArchive);
+library.add(faCoffee, faChild, faCircle, faArchive, faCheckCircle, faPencilAlt, faTimes);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('font-awesome-layers', FontAwesomeLayers);
 Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
