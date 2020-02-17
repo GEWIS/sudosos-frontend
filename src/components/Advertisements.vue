5a2041e1e4bf181ab52e24fe371b07689a330474
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2020-02-17 15:18:55 +0100

    Created a Advertisements.vue component that creates a table of width 100% for all Advertisement objects

---
+++ src/components/Advertisements.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Thumbnail</th>
+          <th>Duur (ms)</th>
+          <th>Actief</th>
+          <th>Toegevoegd op</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="ad.id" v-for="ad in advertisements">
+          <td><img class="thumbnail" :src="ad.thumbnail" alt=""></td>
+          <td>{{ ad.duration }}</td>
+          <td><font-awesome-icon icon="check-circle" /></td>
+          <td>{{ ad.added.toLocaleString() }}</td>
+          <td><a href="https://gewis.nl/"><font-awesome-icon icon="pencil-alt" class="font-awesome-icon" /></a></td>
+          <td><a href="https://gewis.nl/"><font-awesome-icon icon="times" class="font-awesome-icon" /></a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Advertisements',
+  props: ['advertisements'],
+};
+</script>
+
+<style scoped lang="scss">
+  @import '../styles/global/_table.scss';
+
+  .thumbnail {
+    width: 128px;
+    height: 72px;
+  }
+</style>
