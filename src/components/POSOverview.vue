542412ffa12638eb2bda781be63d1679302aa460
Ralf Eemers <ralf-eemers@hotmail.com>
2020-02-19 14:35:57 +0100

    POS Overview start
    Decent layout for the POS overview page

--- src/components/POSOverview.vue
+++ src/components/POSOverview.vue
@@ -1,12 +1,64 @@
 <template>
     <div>
         <h1>Overview</h1>
-        <div class="componentBox">
-            <div class="ownPos">
-                <h2>My Points of Sale</h2>
+        <div id="component-box">
+            <div id="own-pos">
+                <b-card class="pos-card">
+                    <b-card-title class="box-title">
+                        my points of sale
+                    </b-card-title>
+                    <b-card-body>
+                        <div id="pos-lable">
+                            <b-card>
+                                <div class="pos-display">
+                                    Bar POS
+                                </div>
+                                <div class="pos-display">
+                                    POS 1
+                                </div>
+                                <div class="pos-display">
+                                    POS 2
+                                </div>
+                            </b-card>
+                        </div>
+                    </b-card-body>
+                </b-card>
+                <b-card class="page-selecter">
+                    <b-card-body>
+                        <div class="page-display">
+                            PAGE 1/1
+                        </div>
+                    </b-card-body>
+                </b-card>
             </div>
-            <div class="requestPos">
-                <h2>Requested Points of Sale</h2>
+            <div id="request-pos">
+                <b-card class="pos-card">
+                    <b-card-title class="box-title">
+                        requested points of sale
+                    </b-card-title>
+                    <b-card-body>
+                        <div id="pos-lable">
+                            <b-card>
+                                <div class="pos-display">
+                                    POS Request 1
+                                </div>
+                                <div class="pos-display">
+                                    POS Request 2
+                                </div>
+                                <div class="pos-display">
+                                    POS Request 3
+                                </div>
+                            </b-card>
+                        </div>
+                    </b-card-body>
+                </b-card>
+                <b-card class="page-selecter">
+                    <b-card-body>
+                        <div class="page-display">
+                            PAGE 1/1
+                        </div>
+                    </b-card-body>
+                </b-card>
             </div>
         </div>
     </div>
@@ -22,4 +74,41 @@ export default class POSOverview extends Vue {
 </script>
 
 <style scoped lang="scss">
+    @import './src/styles/Card.scss';
+
+    #component-box {
+        display: flex;
+        justify-content: center;
+    }
+
+    #own-pos {
+        padding-right: 15px;
+    }
+
+    .pos-card {
+        width: 35rem;
+        height: 35rem;
+    }
+
+    .pos-display {
+        background-color: #f0f0f0;
+        font-weight: bold;
+
+        margin-bottom: .5rem;
+        padding-top: 1rem;
+        padding-left: 1rem;
+        padding-bottom: 1rem;
+    }
+
+    .page-selecter {
+        background-color: #f0f0f0;
+    }
+
+    .page-display {
+        font-weight: bold;
+    }
+
+    .box-title {
+        font-weight: bold;
+    }
 </style>
