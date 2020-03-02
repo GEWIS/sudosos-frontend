3351e300c4067b60d99928a45747f86a4559c2b0
Roy (Laptop) <r.t.b.kakkenberg@student.tue.nl>
2020-03-02 19:24:15 +0100

    Created a simple btable for advertisement objects on the advertisements page

--- src/components/AdvertisementsList.vue
+++ src/components/AdvertisementsList.vue
@@ -1,48 +1,105 @@
 <template>
   <div>
-    <table class="table table-striped">
-      <thead>
-      <tr>
-        <th>Thumbnail</th>
-        <th>Duur (ms)</th>
-        <th>Actief</th>
-        <th>Toegevoegd op</th>
-        <th style="width: 0"></th>
-        <th style="width: 0"></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-bind:key="ad.id" v-for="ad in advertisements">
-        <td><img class="thumbnail" :src="ad.thumbnail" alt=""></td>
-        <td>{{ ad.duration }}</td>
-        <td>
-          <font-awesome-icon icon="check-circle"/>
-        </td>
-        <td>{{ ad.added.toLocaleString() }}</td>
-        <td><a href="https://gewis.nl/">
-          <font-awesome-icon icon="pencil-alt" class="font-awesome-icon"/>
-        </a></td>
-        <td><a href="https://gewis.nl/">
-          <font-awesome-icon icon="times" class="font-awesome-icon"/>
-        </a></td>
-      </tr>
-      </tbody>
-    </table>
+    <b-card>
+      <b-card-title>
+        Advertenties
+      </b-card-title>
+      <b-card-body>
+        <b-table stacked="sm" small borderless thead-class="table-header"
+                 :items="advertisementList" :fields="fields">
+          <template v-slot:cell(active)="data">
+            <font-awesome-icon :icon="data.value"></font-awesome-icon>
+          </template>
+          <template v-slot:cell(thumbnail)="data">
+            <img class="thumbnail" :src="data.value" alt="Thumbnail">
+          </template>
+        </b-table>
+      </b-card-body>
+    </b-card>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Advertisements',
-  props: ['advertisements'],
-};
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Advertisement } from '@/entities/Advertisement';
+import { User } from '@/entities/User';
+
+function fetchAdvertisements() : Advertisement[] {
+  const advertisements = [{
+    id: '001',
+    thumbnail: 'https://www.dafont.com/forum/attach/orig/5/6/561721.jpg',
+    duration: 3600,
+    active: true,
+    added: new Date(),
+  } as Advertisement,
+  {
+    id: '002',
+    thumbnail: 'https://www.dafont.com/forum/attach/orig/5/6/561721.jpg',
+    duration: 3600,
+    active: true,
+    added: new Date(),
+  } as Advertisement,
+  {
+    id: '003',
+    thumbnail: 'https://www.dafont.com/forum/attach/orig/5/6/561721.jpg',
+    duration: 3600,
+    active: false,
+    added: new Date(),
+  } as Advertisement,
+  ] as Advertisement[];
+
+  return advertisements.slice(0, 3);
+}
+
+  @Component
+export default class RecentAdvertisements extends Vue {
+    @Prop({ type: Object as () => User }) private user!: User;
+
+    advertisementList: Advertisement[] = [];
+
+    // getTimeString = (value: Date) => value.
+
+    getTimeString = (value: Date) => `${this.parseTime(value.getDate())}-${this.parseTime(value.getMonth() + 1)}-${value.getFullYear()} - ${this.parseTime(value.getHours())}:${this.parseTime(value.getMinutes())}`;
+
+    getCheckmark = (value: Boolean) => { if (value) { return 'check-circle'; } return ''; };
+
+    fields: Object[] = [
+      {
+        key: 'thumbnail',
+        label: 'Thumbnail',
+      },
+      {
+        key: 'duration',
+        label: 'Duur (ms)',
+      },
+      {
+        key: 'active',
+        label: 'Actief',
+        formatter: (value: Boolean) => this.getCheckmark(value),
+      },
+      {
+        key: 'added',
+        label: 'Toegevoegd op',
+        formatter: (value: Date) => this.getTimeString(value),
+      },
+    ];
+
+    beforeMount() {
+      this.advertisementList = fetchAdvertisements();
+    }
+
+    parseTime = function parseTime(value: number): string {
+      return (value < 10 ? '0' : '') + value;
+    }
+}
+
 </script>
 
 <style scoped lang="scss">
-  @import '../styles/global/_table.scss';
+  @import './src/styles/Card.scss';
 
   .thumbnail {
-    width: 64px;
-    height: 36px;
+    width: 4rem;
+    height: 2.25rem;
   }
 </style>
--- src/views/Admin/Advertisements.vue
+++ src/views/Admin/Advertisements.vue
@@ -1,14 +1,18 @@
 <template>
   <b-container fluid="md">
     <h1>Advertisements page</h1>
+    <AdvertisementsList></AdvertisementsList>
   </b-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import AdvertisementsList from '@/components/AdvertisementsList.vue';
 
   @Component({
-    components: {},
+    components: {
+      AdvertisementsList,
+    },
   })
 
 export default class Advertisements extends Vue {
