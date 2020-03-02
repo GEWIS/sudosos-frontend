09eb64df07c5fc9e7b7655020e8959e8b7cc79c5
Marcin van de Ven <marcinvandeven@gmail.com>
2020-03-02 15:08:42 +0100

    Created container component
    Separated the containers from the containers into a new component, such that multiple instances can be used.

---
+++ src/components/Container.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container-block">
+    <div class="container-head container d-inline-flex">
+      <b-input-group>
+        <b-form-checkbox inline id="cb_container1" class="cb_space"></b-form-checkbox>
+        <div v-b-toggle.container1>
+          <div class="d-inline">
+            <span>{{ this.name }}</span>
+          </div>
+          <div class="d-inline text-right">
+            <font-awesome-icon
+              pull="right"
+              icon="caret-down">
+            </font-awesome-icon>
+          </div>
+        </div>
+      </b-input-group>
+    </div>
+    <b-collapse id="container1" class="mt-1">
+      <p>Some content</p>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  data() {
+    return {
+      name: 'TestContainer',
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .container-head {
+    margin-bottom: 10px;
+    background-color: silver;
+  }
+  .cb_space {
+    margin-right: 0.1rem;
+  }
+</style>
--- src/components/Containers.vue
+++ src/components/Containers.vue
@@ -2,37 +2,20 @@
   <div class="container-box">
     <h4>Containers</h4>
     <b-form-group>
-        <div class="container-head container d-inline-flex">
-          <b-input-group>
-            <b-form-checkbox inline id="cb_container_1"></b-form-checkbox>
-            <div v-b-toggle.container-1>
-              <div class="d-inline">
-              <span>Container 1</span>
-              </div>
-              <div class="d-inline text-right">
-                <font-awesome-icon
-                  pull="right"
-                  icon="caret-down">
-                </font-awesome-icon>
-              </div>
-          </div>
-          </b-input-group>
-        </div>
-        <b-collapse id="container-1" class="mt-1">
-          <p>Some content</p>
-        </b-collapse>
-        <div class="container-head" v-b-toggle.container-2>Container 2</div>
-        <b-collapse id="container-2" class="mt-1">
-          <p>Some other content</p>
-        </b-collapse>
+    <container con_id="1"></container>
+
+    <container con_id="2"></container>
     </b-form-group>
   </div>
 
 </template>
 
 <script>
+import Container from '@/components/Container.vue';
+
 export default {
   name: 'Containers',
+  components: { Container },
 };
 </script>
 
@@ -42,9 +25,5 @@ export default {
     border-style: solid;
     border-color: #D40000;
     width: 350px;
-  }
-  .container-head {
-    margin-bottom: 10px;
-    background-color: silver;
   }
 </style>
