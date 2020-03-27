6e7b1b6458a794fd3ba96ad3093bde877d4802ab
Marcin van de Ven <marcinvandeven@gmail.com>
2020-03-27 14:40:52 +0100

    Added the items
    Added a placeholding for the items in the containers

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -2,6 +2,7 @@
   <div class="container-block">
     <div class="container-head container d-inline-flex">
       <b-input-group>
+        <!-- TODO: Implement selecting -->
         <b-form-checkbox inline :id="'cb_' + containerId" class="cb_space"></b-form-checkbox>
         <div v-b-toggle="containerId" v-on:click="isOpen = !isOpen">
           <div class="d-inline">
@@ -15,7 +16,18 @@
       </b-input-group>
     </div>
     <b-collapse :id="containerId" class="mt-1">
-      <p>Some content</p>
+      <b-container fluid="md">
+        <!-- TODO: fix images -->
+        <b-row>
+          <b-col v-for="item in items" :key="item.id" class="itemSpace">
+            {{ item.name }}
+            <img v-bind:src="item.img" />
+          </b-col>
+          <!--<b-col> (this is to test the compiling)
+            <img src="../assets/img/beugel.png" height="15%"/>
+          </b-col>-->
+        </b-row>
+      </b-container>
     </b-collapse>
   </div>
 </template>
@@ -26,10 +38,17 @@ export default {
     return {
       containerId: this.value,
       isOpen: false,
+      items: [
+        { id: 1, name: 'beugel', img: '../assets/img/beugel.png' },
+        { id: 2, name: 'radler (laf!)', img: '../assets/img/beugel.png' },
+        { id: 3, name: 'cola', img: '../assets/img/beugel.png' },
+        { id: 4, name: 'ijsthee', img: '../assets/img/beugel.png' },
+        { id: 5, name: 'beugel', img: '../assets/img/beugel.png' },
+        { id: 6, name: 'radler (laf!)', img: '../assets/img/beugel.png' },
+        { id: 7, name: 'cola', img: '../assets/img/beugel.png' },
+        { id: 8, name: 'ijsthee', img: '../assets/img/beugel.png' },
+      ],
     };
-  },
-  mounted() {
-    console.log(this.value);
   },
   props: ['value'],
 };
@@ -37,10 +56,16 @@ export default {
 
 <style scoped>
   .container-head {
-    margin-bottom: 10px;
     background-color: silver;
+    margin-top: 10px;
   }
   .cb_space {
     margin-right: 0.1rem;
+  }
+
+  .itemSpace {
+    background-color: #DFDFDF;
+    border: 1px solid #1b4b72;
+    margin: 0.1em;
   }
 </style>
