69b432f85f6c95db7ebc36bb394505536ff7c91c
BuildTools <ricktrein@hotmail.com>
2020-04-18 13:31:58 +0200

    Added products field to Storage

--- src/entities/Storage.ts
+++ src/entities/Storage.ts
@@ -1,7 +1,10 @@
+import { Product } from '@/entities/Product';
+
 export interface Storage {
     name: String;
     id: String;
     ownerId: String;
+    products: Product[];
     createdAt: Date;
     updatedAt: Date;
     deletedAt?: Date;
