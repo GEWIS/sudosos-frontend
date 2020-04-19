75e1c90e7d750a46c84d42dafa03aae3b9a7ca2e
BuildTools <ricktrein@hotmail.com>
2020-04-19 15:51:12 +0200

    Created the container component and the PointOfSaleRequest view

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="container-block">
-    <div class="container-head px-3 d-flex">
-      <b-input-group class="w-auto my-auto">
-        <b-form-checkbox :id="'cb_' + container.id" class="cb">
-          {{ container.id }}
-          <font-awesome-icon icon="pencil-alt" v-show="container.editable"
-                             class="ml-1" size="sm">
-          </font-awesome-icon>
-        </b-form-checkbox>
+    <div class="container-head px-3 d-flex" v-on:click="isOpen = !isOpen"
+           v-b-toggle="'container_' + container.id">
+      <b-input-group class="my-auto" @click.stop="() => {}">
+        <b-form-checkbox :id="'checkbox_' + container.id" class="checkbox"
+          v-model="checkBoxValue" :disabled="!enabled"/>
+          <span>{{ container.name }} </span>
+          <span class="edit-button" v-show="canEdit && enabled">Edit ✎</span>
       </b-input-group>
 
-      <div class="d-inline ml-2 w-100 my-auto"
-           v-on:click="isOpen = !isOpen"
-           v-b-toggle="container.con_id">
+      <div class="d-inline ml-2 w-100 my-auto">
         <font-awesome-icon pull="right"
                            icon="angle-down"
                            v-show="!isOpen"
@@ -29,14 +26,14 @@
     </div>
 
     <!-- The container itself -->
-    <b-collapse :id="container.con_id" class="mt-1">
+    <b-collapse :id="'container_' + container.id" class="mt-1">
         <b-row class="mx-0">
-          <b-col v-for="item in container.items"
+          <b-col v-for="item in container.products"
                  :key="item.id"
                  class="text-center product-card px-2"
                  cols="6" sm="4" md="3" lg="2">
             <div class="product">
-              <img src="../assets/img/beugel.png" :alt="item.name"/>
+              <img :src="item.picture" :alt="item.name"/>
               <p class="w-100 product-name mb-0">{{ item.name }}</p>
             </div>
           </b-col>
@@ -54,17 +51,76 @@ export default class Container extends Vue {
   @Prop()
   container: Storage | undefined;
 
+  @Prop()
+  enabled: Boolean | undefined;
+
   isOpen: Boolean = false;
 
-  get editable() {
-    if (this.container) {
-      return this.$store.state.currentUser.organs.includes(this.container.ownerId);
-    }
-    return false;
+  selected: Boolean = false;
+
+  get checkBoxValue() {
+    return this.selected || !this.enabled;
+  }
+
+  set checkBoxValue(newValue: Boolean) {
+    this.selected = newValue;
+  }
+
+  get canEdit() {
+    return this.container
+      ? this.$store.state.currentUser.organs.includes(this.container.ownerId) : false;
   }
 }
 </script>
 
 <style scoped lang="scss">
-  @import "./src/styles/Containers.scss";
+.em-title {
+  color: $gewis-red;
+  text-transform: uppercase;
+}
+
+.container-block {
+  margin-bottom: 0.5rem;
+}
+
+.container-head {
+  background-color: $gewis-grey-light;
+  cursor: pointer;
+  height: 2.5rem;
+  span {
+      white-space: nowrap;
+  }
+
+  .edit-button {
+      background-color: $gewis-grey;
+      margin-left: 8px;
+      border-radius: 4px;
+      padding-left: 4px;
+      padding-right: 4px;
+  }
+}
+
+.chec {
+  display: inline;
+}
+
+.product-card {
+  margin: 0.5rem 0;
+
+  .product {
+    background-color: $gewis-grey-light;
+
+    > img {
+      width: auto;
+      height: auto;
+      max-height: 5rem;
+      background-color: $gewis-grey-light;
+    }
+  }
+}
+
+.product-name {
+  background: $gewis-grey-accent;
+}
+
 </style>
--- src/views/PointOfSale/PointOfSaleRequest.vue
+++ src/views/PointOfSale/PointOfSaleRequest.vue
@@ -1,21 +1,170 @@
 <template>
   <b-container fluid="lg">
-  <h1 class="mb-2 mb-sm-3 mb-lg-4">{{ $t('posRequest.Point of Sale request') }}</h1>
+    <b-row>
+      <b-col md="8" sm="12">
+        <h1 class="mb-2 mb-sm-2 mb-lg-2">
+          {{ $t('posRequest.Request:') }} {{ requestedPOS.name }}
+        </h1>
+      </b-col>
+      <b-col class="approve-reject-buttons" md="4" sm="12">
+        <b-button variant="success">✅ {{ $t('posRequest.Approve')}}</b-button>
+        <b-button variant="danger">❌ {{ $t('posRequest.Reject')}}</b-button>
+      </b-col>
+    </b-row>
+    <hr>
+    <b-row>
+      <b-col md="3" sm="12">
+        <h3>{{ $t('posRequest.General') }}</h3>
+        <b>{{ $t('posRequest.Title') }}</b>
+        <p>{{ requestedPOS.name }}</p>
+        <h3>{{ $t('posRequest.Management') }}</h3>
+        <b>{{ $t('posRequest.Owner') }}</b>
+        <p>{{ ownerData.organName }}</p>
+        <b>{{ $t('posRequest.Managers') }}</b>
+        <ul>
+          <li v-for="member in ownerData.members" v-bind:key="member">
+            {{ member }}
+          </li>
+        </ul>
+      </b-col>
+      <b-col md="9" sm="12" class="containers-container">
+        <p class="containers-header">{{ $t('Containers') }}</p>
+        <Container v-for="storage in requestedPOS.storages" v-bind:key="storage.id"
+          :container="storage" :enabled="false"/>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Container from '@/components/Container.vue';
+import { Storage } from '@/entities/Storage';
+import { Product } from '@/entities/Product';
+import { PointOfSale } from '@/entities/PointOfSale';
 
   @Component({
     components: { Container },
   })
 
 export default class PointOfSaleRequest extends Vue {
+  // *************************************************
+  //
+  //               Begin test data
+  //
+  // *************************************************
+  private beugel: Product = {
+    id: '1',
+    name: 'Grolsch beugel',
+    ownerId: '1',
+    price: 110,
+    picture: 'https://www.supermarktaanbiedingen.com/public/images/product/2017/39/0-508102fls-grolsch-premium-pilsner-beugel-45cl.jpg',
+    traySize: 20,
+    category: 'drink',
+    isAlcoholic: true,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  private tripel: Product = {
+    id: '2',
+    name: 'Grimbergen tripel (voor de sfeer)',
+    ownerId: '1',
+    price: 90,
+    picture: 'https://deklokdranken.blob.core.windows.net/product-images/105120.jpg',
+    traySize: 24,
+    category: 'drink',
+    isAlcoholic: true,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  private alcoholFree: Product = {
+    id: '3',
+    name: 'Alcoholvrije Athena-meuk',
+    ownerId: '2',
+    price: 50,
+    picture: 'https://www.cocktailicious.nl/wp-content/uploads/2019/10/sex-on-the-beach.jpg',
+    traySize: 1,
+    category: 'drink',
+    isAlcoholic: false,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  private cocktail: Product = {
+    id: '4',
+    name: 'Athena-meuk met alcohol',
+    ownerId: '2',
+    price: 150,
+    picture: 'https://www.mitra.nl/cms/userfiles/cocktails/298-mojito43.png',
+    traySize: 1,
+    category: 'drink',
+    isAlcoholic: true,
+    negative: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+
+  private bacFridge: Storage = {
+    name: 'BAC-koelkast',
+    id: '1',
+    ownerId: '1',
+    products: [this.beugel, this.tripel],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+
+  private outdoorCocktails: Storage = {
+    name: 'Athena-cocktails',
+    id: '2',
+    ownerId: '2',
+    products: [this.alcoholFree, this.cocktail],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+
+  private requestedPOS: PointOfSale = {
+    name: 'SudoSOS-tablet',
+    id: '1',
+    ownerId: '1',
+    storages: [this.bacFridge, this.outdoorCocktails],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+
+  private ownerData: Object = {
+    organName: 'Fictieve sjaarzencommissie',
+    members: ['Sjaars 1', 'Sjaars -1', 'Sjaars 65', 'Marcin van de Ven'],
+  }
+
+  // *************************************************
+  //
+  //               End test data
+  //
+  // *************************************************
 }
 </script>
 
 <style lang="scss" scoped>
+  .approve-reject-buttons{
+    button {
+      margin-right: 8px;
+      min-width: 100px;
+    }
+  }
+  .containers-container{
+    border: 2px solid $gewis-grey-light;
 
+    .containers-header {
+      color: $gewis-red;
+      font-size: 1em;
+      font-weight: 600;
+      text-transform: uppercase;
+      margin: 1em;
+    }
+  }
 </style>
