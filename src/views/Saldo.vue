713474dded68684382ad61625b6823221cb161e6
Ruben Brinkman <rubenjulianbrinkman@gmail.com>
2021-10-17 14:22:07 +0200

    StripeJS added

--- src/views/Saldo.vue
+++ src/views/Saldo.vue
@@ -1,16 +1,15 @@
 <template>
   <b-container fluid="lg">
-  <h1 class="mb-2 mb-sm-3 mb-lg-4">{{ $t('saldo.Saldo') }}</h1>
+    <h1 class="mb-2 mb-sm-3 mb-lg-4">{{ $t('saldo.Saldo') }}</h1>
   </b-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-  @Component({
-    components: {},
-  })
-
+@Component({
+  components: {},
+})
 export default class Saldo extends Vue {
 }
 </script>
