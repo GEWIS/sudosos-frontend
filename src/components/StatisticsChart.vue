ce96e8977d34d7c5b9a1ca7726430c62227ffb04
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-02-24 17:19:29 +0100

    - Extracted StatisticsChart component from ConsumedProducts view.

---
+++ src/components/StatisticsChart.vue
@@ -0,0 +1,118 @@
+<template>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script>
+
+import { Bar } from 'vue-chartjs/legacy';
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+export default {
+  name: 'StatisticsChart',
+  components: {
+    Bar,
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart',
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label',
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 400,
+    },
+    cssClasses: {
+      default: '',
+      type: String,
+    },
+    styles: {
+      type: Object,
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      default: () => {},
+    },
+    plugins: {
+      type: Array,
+      default: () => [Title],
+    },
+    section: {
+      type: String,
+      default: 'consumed',
+    },
+    timeScale: {
+      type: String,
+      default: 'year',
+    },
+  },
+  data() {
+    const label = 'january';
+    return {
+      chartData: {
+        labels: [
+          label,
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Hello World!',
+            align: 'start',
+          },
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/ConsumedProducts.vue
+++ src/views/ConsumedProducts.vue
@@ -1,98 +1,17 @@
 <template>
-  <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />
+  <b-container fluid="lg">
+    <h1 class="mb-2 mb-sm-3 mb-lg-4">Statistics</h1>
+    <StatisticsChart time-scale="" />
+  </b-container>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs/legacy';
-
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from 'chart.js';
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+import StatisticsChart from '@/components/StatisticsChart.vue';
 
 export default {
-  name: 'BarChart',
+  name: 'ConsumedProducts',
   components: {
-    Bar,
-  },
-  props: {
-    chartId: {
-      type: String,
-      default: 'bar-chart',
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label',
-    },
-    width: {
-      type: Number,
-      default: 400,
-    },
-    height: {
-      type: Number,
-      default: 400,
-    },
-    cssClasses: {
-      default: '',
-      type: String,
-    },
-    styles: {
-      type: Object,
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      default: () => {},
-    },
-    plugins: {
-      type: Array,
-      default: () => [],
-    },
-  },
-  data() {
-    return {
-      chartData: {
-        labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December',
-        ],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-      },
-    };
+    StatisticsChart,
   },
 };
 </script>
