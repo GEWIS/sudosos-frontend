9d131dd843342e558fe5a31915f1c7211792c9dc
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-02-24 15:55:01 +0100

    - Added example bar chart. - Added consumed_products page with route. - Added chart.js and Vue-chartjs libraries.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import BootstrapVue from 'bootstrap-vue';
+import dinero from 'dinero.js';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome';
+import { faCoffee, faChild, faCircle, faArchive, faCheckCircle, faPencilAlt, faTimes, faPlus, faInfoCircle, faChevronLeft, faChevronRight, faTimesCircle, faFileExport, faAngleUp, faAngleDown, faPenAlt, faGlobeEurope, faCheck, faQuestion, faTrash, } from '@fortawesome/free-solid-svg-icons';
+import vSelect from 'vue-select';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
+import languages from './locales/index';
+import 'vue-select/dist/vue-select.css';
+// Import the BootstrapVue style
+import './styles/global/_main.scss';
+library.add(faCoffee, faChild, faCircle, faCheck, faArchive, faCheckCircle, faPencilAlt, faTimes, faPlus, faCheck, faInfoCircle, faChevronLeft, faChevronRight, faTimesCircle, faFileExport, faAngleUp, faAngleDown, faPenAlt, faTrash, faGlobeEurope, faQuestion);
+// Default settings for Dinero
+dinero.defaultCurrency = 'EUR';
+dinero.defaultPrecision = 2;
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('font-awesome-layers', FontAwesomeLayers);
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
+Vue.component('v-select', vSelect);
+Vue.use(VueI18n);
+Vue.use(BootstrapVue);
+const messages = Object.assign(languages);
+const i18n = new VueI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages,
+});
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
 new Vue({
-  router,
-  store,
-  render: h => h(App),
+    router,
+    store,
+    i18n,
+    render: (h) => h(App),
 }).$mount('#app');
-// # sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,216 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Public from '@/layout/Public.vue';
+import Dashboard from '@/layout/Dashboard.vue';
+import Home from '@/views/Home.vue';
+import Saldo from '@/views/Saldo.vue';
+import Transactions from '@/views/Transactions.vue';
+import PointOfSaleOverview from '@/views/PointOfSale/PointOfSaleOverview.vue';
+import PointOfSaleApprove from '@/views/PointOfSale/PointOfSaleApprove.vue';
+import PointOfSaleRequest from '@/views/PointOfSale/PointOfSaleCreate.vue';
+import ManagePointOfSale from '@/views/Admin/ManagePointOfSale.vue';
+import Screens from '@/views/Admin/Screens.vue';
+import Banners from '@/views/Admin/Banners.vue';
+import FlaggedTransactions from '@/views/BAC/FlaggedTransactions.vue';
+import ManageProducts from '@/views/BAC/ManageProducts.vue';
+import Profile from '@/views/User/Profile.vue';
+import FlaggedTransactionDetails from '@/views/BAC/FlaggedTransactionDetails.vue';
+import PointOfSaleInfo from '@/views/PointOfSale/PointOfSaleInfo.vue';
+import BorrelkaartOverview from '@/views/BAC/Borrelkaarten.vue';
+import BorrelkaartenPrint from '@/views/BAC/BorrelkaartenPrint.vue';
+import TransactionEditPage from '@/views/BAC/TransactionEditPage.vue';
+import UserOverview from '@/views/BAC/UserOverview.vue';
+import UserDetails from '@/views/BAC/UserDetails.vue';
+import Login from '@/views/Login.vue';
+import PasswordReset from '@/views/PasswordReset.vue';
+import APIHelper from '@/mixins/APIHelper';
+import Logout from '@/mixins/Logout';
+import PointOfSaleEdit from '@/views/PointOfSale/PointOfSaleEdit.vue';
+import TermsOfService from '@/views/TermsOfService.vue';
+import { TermsOfServiceStatus } from '@/entities/User';
+import ConsumedProducts from '@/views/ConsumedProducts.vue';
 
 Vue.use(VueRouter);
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home,
+    path: '',
+    component: Public,
+    children: [
+      {
+        path: '/login',
+        component: Login,
+        name: 'login',
+      },
+      {
+        path: '/tos',
+        component: TermsOfService,
+        name: 'termsOfService',
+      },
+      {
+        path: '/passwordreset',
+        component: PasswordReset,
+        name: 'passwordReset',
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '',
+    component: Dashboard,
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/transactions',
+        name: 'transactions',
+        component: Transactions,
+      },
+      {
+        path: '/saldo',
+        name: 'saldo',
+        component: Saldo,
+      },
+      {
+        path: '/point-of-sale/overview',
+        name: 'pointOfSale',
+        component: PointOfSaleOverview,
+      },
+      {
+        path: '/point-of-sale/request',
+        name: 'pointOfSaleRequest',
+        component: PointOfSaleRequest,
+      },
+      {
+        path: '/point-of-sale/approve/:id',
+        name: 'pointOfSaleApprove',
+        component: PointOfSaleApprove,
+        props: true,
+      },
+      {
+        path: '/point-of-sale/info/:id',
+        name: 'pointOfSaleInfo',
+        component: PointOfSaleInfo,
+        props: true,
+      },
+      {
+        path: '/point-of-sale/edit/:id',
+        name: 'pointOfSaleEdit',
+        component: PointOfSaleEdit,
+        props: true,
+      },
+      {
+        path: '/manage-pos',
+        name: 'managePointOfSale',
+        component: ManagePointOfSale,
+      },
+      {
+        path: '/screens',
+        name: 'screens',
+        component: Screens,
+      },
+      {
+        path: '/banners',
+        name: 'banners',
+        component: Banners,
+      },
+      {
+        path: '/flagged-transactions',
+        name: 'flaggedTransactions',
+        component: FlaggedTransactions,
+      },
+      {
+        path: '/flagged-transactions/flag/:id',
+        name: 'flaggedTransactionDetails',
+        component: FlaggedTransactionDetails,
+        props: true,
+      },
+      {
+        path: '/user-overview',
+        name: 'userOverview',
+        component: UserOverview,
+      },
+      {
+        path: '/user-details/user/:id',
+        name: 'userDetails',
+        component: UserDetails,
+        props: true,
+      },
+      {
+        path: '/manage-products',
+        name: 'manageProducts',
+        component: ManageProducts,
+      },
+      {
+        path: '/borrelkaart-overview',
+        name: 'borrelkaartOverview',
+        component: BorrelkaartOverview,
+      },
+      {
+        path: '/borrelkaarten-overview/cards/:id',
+        name: 'borrelkaartenPrint',
+        component: BorrelkaartenPrint,
+        props: true,
+      },
+      {
+        path: '/transaction/:id',
+        name: 'transactionEditPage',
+        component: TransactionEditPage,
+        props: true,
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: Profile,
+      },
+      {
+        path: '/consumed_products',
+        name: 'consumed_products',
+        component: ConsumedProducts,
+      },
+    ],
   },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
 ];
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+let previousFrom = null;
+router.beforeEach((to, from, next) => {
+  const token = APIHelper.getToken();
+  const notToLogin = to.name !== 'login' && to.name !== 'passwordReset';
+  const jwtIsNull = token === undefined
+        || token.jwtToken === undefined
+        || token.jwtExpires === undefined;
+  const jwtIsExpired = new Date(Number(token?.jwtExpires)) < new Date();
+  // if (from.name === previousFrom && to.name !== 'login') {
+  //   console.warn('previousFrom');
+  //   Logout.logout(next);
+  //   return;
+  // }
+  const notToAcceptToS = to.name !== 'termsOfService';
+  const user = APIHelper.getTokenUser();
+  const acceptedToS = user !== undefined
+        && (user.acceptedToS === TermsOfServiceStatus.ACCEPTED
+            || user.acceptedToS === TermsOfServiceStatus.NOT_REQUIRED);
+  previousFrom = from.name;
+  if ((notToLogin && jwtIsNull) || (!jwtIsNull && jwtIsExpired)) {
+    Logout.logout(next);
+  } else if (user !== undefined && notToAcceptToS && !acceptedToS) {
+    next({ name: 'termsOfService' });
+  } else {
+    next();
+  }
 });
 export default router;
 // # sourceMappingURL=index.js.map
