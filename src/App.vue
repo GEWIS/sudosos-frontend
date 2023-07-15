5c52ee57427e0cd18e7c6b34a0154b3c778c7c29
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-07-15 17:58:53 +0200

    Created copyright banner

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import CopyrightBanner from "@/components/CopyrightBanner.vue";
 
 </script>
 
 <template>
-
-
   <RouterView />
+  <CopyrightBanner />
 </template>
---
+++ src/components/CopyrightBanner.vue
@@ -0,0 +1,42 @@
+<template>
+<footer>
+  <span class="copyright">
+    <b>© 2023 GEWIS</b>
+  </span>
+  <span class="separator"> | </span>
+  <a id="tos" href="https://wieditleesttrekteenbak.nl/">Terms of Service</a>
+</footer>
+</template>
+
+<script>
+export default {
+  name: "CopyrightBanner"
+}
+</script>
+
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 0 1rem;
+  height: 60px;
+  line-height: 60px;
+  background-color: #f5f5f5;
+}
+
+b {
+  font-weight: bolder;
+  color: black;
+}
+
+.separator {
+  color: black;
+  margin: 0 1rem;
+}
+
+#tos {
+  color: red;
+}
+</style>
