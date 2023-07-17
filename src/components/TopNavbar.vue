106a943d60eb9983e6da974420fb8e4f169c6bfd
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-07-17 12:42:43 +0200

    Styled some more - Added SudoSOS logo to TopNavbar.vue

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -1,17 +1,23 @@
+
 <template>
-  <div class="navbar">
-    <Menubar :model="leftItems" />
-    <Menubar :model="rightItems" />
+  <div class="container">
+    <nav>
+      <Menubar :model="leftItems" >
+        <template #start>
+          <a id="sudosos" href="https://sudosos.gewis.nl/">SudoSOS<img id="logo" src="../assets/img/gewis-branding.svg"></a>
+
+        </template>
+      </Menubar>
+      <Menubar :model="rightItems" />
+    </nav>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
+// TODO: Style the hovering of buttons
 const leftItems = ref([ // TODO: Implement Submenus
-  {
-    label: 'SudoSOS', // TODO: Do fancy logo
-  },
   {
     label: 'Transactions'
   },
@@ -40,15 +46,24 @@ const rightItems = ref([
 </script>
 
 <style scoped lang="scss">
-.navbar {
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  background-color: #d40000;
+.container {
   position: absolute;
   top: 0;
   left: 0;
+  background-color: #d40000;
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  line-height: 1.5;
+  padding: 0.5rem 1rem;
+}
+nav {
+  display: flex;
+  flex-direction: row;
+  max-width: 1180px;
+  width: 100%;
   justify-content: space-between;
+
 
   .p-menubar {
     background-color: #d40000;
@@ -57,6 +72,23 @@ const rightItems = ref([
 
 :deep(.p-menubar .p-menubar-root-list > .p-menuitem > .p-menuitem-content .p-menuitem-link .p-menuitem-text) {
   color: white;
+  font-family: Raleway, sans-serif;
+  font-weight: 500;
+  font-size: 1rem;
+  padding-right: 5px;
+}
+
+#logo {
+  height: 65px;
+}
+
+#sudosos {
+  color: white;
+  font-weight: 700;
+  font-family: Raleway, sans-serif;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
 </style>
