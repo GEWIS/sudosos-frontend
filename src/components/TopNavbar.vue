4c1807d70d5e01dad972f6f3c5bb1581c391aa44
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-07-17 11:52:49 +0200

    Worked on styling of TopNavbar - Fought with SCSS - Learned about :deep operator - Still don't understand :deep operator

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -1,15 +1,16 @@
 <template>
-<Menubar :model="items">
-
-</Menubar>
+  <div class="navbar">
+    <Menubar :model="leftItems" />
+    <Menubar :model="rightItems" />
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const items = ref([
+const leftItems = ref([ // TODO: Implement Submenus
   {
-    label: 'SudoSOS'
+    label: 'SudoSOS', // TODO: Do fancy logo
   },
   {
     label: 'Transactions'
@@ -21,11 +22,41 @@ const items = ref([
     label: 'Points of Sale'
   },
   {
-    label: 'BAC'
+    label: 'BAC' // TODO: Implement RBAC Determination for permissions
   },
     ])
+
+const rightItems = ref([
+  {
+    label: 'Robin' // TODO: Implement User Getter
+  },
+  {
+    label: '€420.69' // TODO: Implement Balance Getter
+  },
+  {
+    label: 'World'
+  },
+])
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.navbar {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  background-color: #d40000;
+  position: absolute;
+  top: 0;
+  left: 0;
+  justify-content: space-between;
+
+  .p-menubar {
+    background-color: #d40000;
+  }
+}
+
+:deep(.p-menubar .p-menubar-root-list > .p-menuitem > .p-menuitem-content .p-menuitem-link .p-menuitem-text) {
+  color: white;
+}
 
 </style>
