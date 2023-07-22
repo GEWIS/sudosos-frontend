24835147e15f93e6fd3dea026fb47064f8b240a9
Samuel Oosterholt <samuel.oosterholt20001004@gmail.com>
2023-07-22 23:36:47 +0200

    FIxed banner reactivity

--- src/components/Banner/BannerComponent.vue
+++ src/components/Banner/BannerComponent.vue
@@ -6,11 +6,40 @@
 import {useBannerStore} from "@/stores/banner.store";
 import BannerDisplayComponent from "@/components/Banner/BannerDisplayComponent.vue";
 import {storeToRefs} from "pinia";
+import {onUnmounted, watch} from "vue";
 
 const bannerStore = useBannerStore();
 bannerStore.fetchBanners();
 
 const { activeBanners } = storeToRefs(bannerStore);
+
+// Initialize the index and currentBanner with the first active banner
+let bannerIndex = 0;
+let currentBanner = activeBanners.value[bannerIndex];
+
+// Function to switch to the next banner
+const switchToNextBanner = () => {
+  bannerIndex = (bannerIndex + 1) % activeBanners.value.length;
+  currentBanner = activeBanners.value[bannerIndex];
+};
+
+let intervalId: number;
+if (currentBanner) setInterval(switchToNextBanner, currentBanner.duration * 1000);
+
+watch(activeBanners, (newBanners) => {
+  if (newBanners.length > 0) {
+    currentBanner = newBanners[bannerIndex];
+    // Clear the existing interval and start a new one with the updated duration
+    clearInterval(intervalId);
+    intervalId = setInterval(switchToNextBanner, currentBanner.duration * 1000);
+  }
+});
+
+
+// Cleanup the interval when the component is unmounted
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
 
 </script>
 
