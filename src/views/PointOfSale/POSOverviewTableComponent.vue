f7574229bfced0b6d9f972f7408d6295cdee5db1
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-07-23 02:12:00 +0200

    Seemingly completed UI of POSOverview

--- src/views/PointOfSale/POSOverviewTableComponent.vue
+++ src/views/PointOfSale/POSOverviewTableComponent.vue
@@ -4,6 +4,11 @@
       <Column field="title" header="Title"/>
       <Column field="owner" header="Owner"/>
       <Column field="containerAmount" header="Containers"/>
+      <Column headerStyle="width: 3rem; text-align: center" bodyStyle="text-align: center; overflow: visible">
+        <template #body>
+          <Button type="button" severity='danger' icon="pi pi-info-circle" outlined />
+        </template>
+      </Column>
     </DataTable>
   </CardComponent>
 </template>
@@ -13,7 +18,7 @@ import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import {onMounted, ref} from "vue";
 
-const pos = ref([]);
+
 
 onMounted(() => {
   pos.value = [
@@ -34,4 +39,28 @@ onMounted(() => {
     },
   ];
 });
+const pos = ref();
 </script>
+
+<style scoped lang="scss">
+:deep(.p-datatable .p-datatable-thead > tr > th) {
+  background-color: #f8f8f8;
+  border-top: none;
+  text-transform: uppercase;
+  font-family: Lato,Arial,sans-serif!important;
+  font-size: 1rem;
+  padding: 0.375rem 0;
+  line-height: 1.5;
+}
+
+:deep(.p-datatable .p-datatable-tbody > tr) {
+  background-color: #f8f8f8;
+}
+
+:deep(.p-datatable .p-datatable-tbody > tr > td) {
+  border: none;
+  padding: 0.375rem 0;
+  font-size: 1rem;
+  font-family: Lato,Arial,sans-serif!important;
+}
+</style>
--- src/views/PointOfSale/POSOverviewView.vue
+++ src/views/PointOfSale/POSOverviewView.vue
@@ -4,7 +4,7 @@
   <div class="page-container">
     <div class="page-title">Point of Sale Overview</div>
     <div class="content-wrapper">
-      <POSOverviewTableComponent />
+      <POSOverviewTableComponent class="pos-overview-table"/>
     </div>
   </div>
 </template>
@@ -13,3 +13,10 @@
 import POSOverviewTableComponent from "@/views/PointOfSale/POSOverviewTableComponent.vue";
 </script>
 
+<style scoped lang="scss">
+@import "@/styles/BasePage.css";
+
+.pos-overview-table {
+  width: 80%;
+}
+</style>
