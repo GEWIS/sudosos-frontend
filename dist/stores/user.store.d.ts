1f788784abfe3e5e512b79c926b568e240ddeb51
Bartjan Henkemans <bartjan@henkemans.be>
2023-07-25 20:13:00 +0200

    Also pushed the build

--- dist/stores/user.store.d.ts
+++ dist/stores/user.store.d.ts
@@ -1,8 +1,9 @@
-import { BalanceResponse, UserResponse } from "@sudosos/sudosos-client";
+import { BalanceResponse, PaginatedFinancialMutationResponse, UserResponse } from "@sudosos/sudosos-client";
 import { ApiService } from "../services/ApiService";
 interface CurrentState {
     balance: BalanceResponse | null;
     user: UserResponse | null;
+    financialMutations: PaginatedFinancialMutationResponse;
 }
 interface UserModuleState {
     users: UserResponse[];
@@ -16,6 +17,7 @@ export declare const useUserStore: import("pinia").StoreDefinition<"user", UserM
 }, {
     fetchUsers(service: ApiService): Promise<void>;
     fetchCurrentUserBalance(id: number, service: ApiService): Promise<void>;
+    fetchUsersFinancialMutations(id: number, service: ApiService): Promise<void>;
     setCurrentUser(user: UserResponse): void;
     addUser(user: UserResponse): void;
     clearCurrent(): void;
