5b68755ce8a1c3b1c079b6bd29d3d1e2f8358315
Noel van de Vrie <46963141+noelvandevrie@users.noreply.github.com>
2023-10-17 00:50:36 +0200

    Changed DashboardLayout.vue to render the TopNavbar and CopyrightBanner components unless the page we are on is the tos-page.

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -1,13 +1,20 @@
 <template>
-  <TopNavbar v-if="['home'].indexOf($route.name) > -1"/>
+  <TopNavbar v-if="renderComponent"/>
   <RouterView />
-  <CopyrightBanner v-if="['home'].indexOf($route.name) > -1"/>
+  <CopyrightBanner v-if="renderComponent"/>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import CopyrightBanner from "@/components/CopyrightBanner.vue";
 import TopNavbar from "@/components/TopNavbar.vue";
+import router from "@/router";
+import { computed } from "vue";
+
+const renderComponent = computed(() => {
+  return router.currentRoute.value.name !== 'tos';
+});
+
 </script>
 
 <style scoped>
