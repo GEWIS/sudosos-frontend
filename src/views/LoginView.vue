1507895fcb9d57c7db248cb12ef3334bbc9d2276
Noel van de Vrie <46963141+noelvandevrie@users.noreply.github.com>
2023-10-16 19:58:50 +0200

    Feature/loginpage validation (#31)
    * Added validation and styling to the login fields.
    
    * Added validation and styling to the login fields.
    
    * Implemented localLogin as it was missing
    
    * added TODOs and related them to their respective issues, routing of the images should be fixed now and removed redundant check of the loginForm values.
    
    * Added background error color to the fields and fixed the error messages shifting down elements on the page.
    
    * deleted .env 
    
    deleted .env from commit
    
    * Implement all POS management pages. (#16)
    
    * Attempt at fix for product cards
    
    * Added form validation to create container
    - Added acutal form to the AddContainerDialogComponent.vue
    - Added response to createEmptyContainer method
    - Fixed card size for ProductComponent.vue
    
    * Added validation to POSEditView.vue
    
    * Some lint and type-check fixes.
    
    * Small fix from the new common packages.
    
    * Some minor improvements
    
    * Fixed POS Edit page, selectedContainers now actually work.
    
    * Implement adding container functionality
    
    * Fixed form validation resizing input fields
    
    * Some small formatting fixes
    
    * Removed unused console log statements
    
    * Small fixes
    
    * Stylized form validation in adding containers
    
    * Further styling of form validation
    
    * Implemented function to close dialogue on tap anywhere else
    
    ---------
    
    Co-authored-by: Samuel <samuelincognito@gmail.com>
    
    * Added .env-example
    
    * Update README.md
    
    * Added license and notice to the repo
    
    ---------
    
    Co-authored-by: Robin van Dijke <robinvandijke2002@gmail.com>
    Co-authored-by: Samuel <samuelincognito@gmail.com>

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,66 +1,129 @@
 <template>
   <div>
     <main>
-      <img id="login-image" src="../assets/img/bier.png" alt="logo" />
+      <img
+          id="login-image"
+          src="@/assets/img/bier.png"
+          alt="logo"
+      />
       <h1>{{ $t('login.SudoSOS Login') }}</h1>
-      <Button id="login-gewis-button" @click="loginViaGEWIS" severity="success"
-        ><img id="gewis-branding" src="../assets/img/gewis-branding.svg" alt="GEWIS" />{{
-          $t('login.Login via GEWIS')
-        }}</Button
+      <Button
+          id="login-gewis-button"
+          @click="loginViaGEWIS"
+          severity="success"
       >
+        <img
+            id="gewis-branding"
+            src="@/assets/img/gewis-branding.svg"
+            alt="GEWIS"
+        />
+        {{ $t('login.Login via GEWIS') }}
+      </Button>
       <hr />
-      <form id="login-form" @submit.prevent="ldapLogin">
-        <!--      TODO: Form validation with vee-validate -->
-        <label for="username">{{ $t('login.Username') }}</label>
-        <InputText
-          id="username"
-          type="text"
-          v-bind="username"
-          :placeholder="$t('login.Enter username')"
-        />
-        <label for="password">{{ $t('login.Password') }}</label>
-        <InputText
-          id="password"
-          type="password"
-          v-bind="password"
-          :placeholder="$t('login.Enter password')"
-        />
-        <Button type="submit" id="login-button" severity="danger">{{ $t('login.Login') }}</Button>
-        <div class="password-reset" @click="resetPassword">{{ $t('login.Password reset') }}</div>
-      </form>
+      <Form id="login-form" @submit="loginHandler">
+        <label
+            id="input-description"
+            for="username"
+        >
+          {{ $t('login.Username') }}
+        </label>
+        <span class="p-float-label with-error">
+          <InputText
+            id="username"
+            type="text"
+            v-bind="username"
+            size="large"
+            name="username"
+            :class="{'p-invalid': loginForm.errors.value.username}"
+          />
+          <label
+              :class="{'contains-text': username.modelValue }"
+              for="username">{{ $t('login.Username') }}
+          </label>
+          <small class="p-error">
+            <i v-if="loginForm.errors.value.username" class="pi pi-exclamation-circle"/>
+            {{ loginForm.errors.value.username }}
+          </small>
+        </span>
+        <label
+            id="input-description"
+            for="password"
+        >
+          {{ $t('login.Password') }}
+        </label>
+        <span class="p-float-label with-error">
+          <InputText
+              id="password"
+              type="password"
+              v-bind="password"
+              size="large"
+              name="password"
+              :class="{'p-invalid': loginForm.errors.value.password}"
+          />
+          <label
+              :class="{'contains-text': password.modelValue }"
+              for="password">{{ $t('login.Password') }}
+          </label>
+          <smal class="p-error">
+            <i v-if="loginForm.errors.value.password" class="pi pi-exclamation-circle"/>
+            {{ loginForm.errors.value.password }}
+          </smal>
+        </span>
+        <Button
+            type="submit"
+            id="login-button"
+            severity="danger"
+        >
+          {{ $t('login.Login') }}
+        </Button>
+        <div
+            class="password-reset"
+            @click="resetPassword"
+        >
+          {{ $t('login.Password reset') }}
+        </div>
+      </Form>
     </main>
-    <CopyrightBanner />
+    <CopyrightBanner/>
   </div>
 </template>
 
 <script setup lang="ts">
 import CopyrightBanner from "@/components/CopyrightBanner.vue";
+import { useRoute } from "vue-router";
 import { onBeforeMount } from "vue";
 import { useUserStore, useAuthStore } from "@sudosos/sudosos-frontend-common";
 import apiService from "@/services/ApiService";
-import { useRoute } from "vue-router";
-import { v4 as uuid } from 'uuid';
 import router from "@/router";
-import { useForm } from "vee-validate";
-import * as yup from 'yup';
+import { v4 as uuid } from 'uuid';
+import { useForm, Form } from "vee-validate";
 import { toTypedSchema } from '@vee-validate/yup';
+import * as yup from 'yup';
+import InputText from "primevue/inputtext";
 
 const authStore = useAuthStore();
 const userStore = useUserStore();
+
 const schema = toTypedSchema(
   yup.object({
-    username: yup.string().required(),
-    password: yup.string().required()
+    username: yup
+        .string()
+        .required("This is a required field."),
+    password: yup
+        .string()
+        .required("This is a required field.")
   })
 );
-const { values, defineComponentBinds } = useForm({
+const loginForm = useForm({
   validationSchema: schema
 });
-const username = defineComponentBinds('username');
-const password = defineComponentBinds('password');
+
+const username = loginForm.defineComponentBinds('username');
+const password = loginForm.defineComponentBinds('password');
 
 const route = useRoute();
 
+//TODO error handling and error toasts #18
 onBeforeMount(() => {
   if (route.query.token !== undefined) {
     const token = route.query.token as string;
@@ -70,16 +133,30 @@ onBeforeMount(() => {
   }
 });
 
-const ldapLogin = async (event: Event) => {
-  event.preventDefault();
-  if (!values.username || !values.password) return;
-  await authStore.gewisLdapLogin(values.username, values.password, apiService).then(() => {
-    if (authStore.getUser) userStore.fetchCurrentUserBalance(authStore.getUser.id, apiService);
-    router.push({ name: 'home' });
-  }).catch((error) => {
-    console.error(error);
-  });
-};
+//TODO error handling and error toasts #18
+const loginHandler = loginForm.handleSubmit(async () => {
+  if (username.value.modelValue.includes('@')) {
+    await apiService.authenticate.localAuthentication({
+      accountMail: username.value.modelValue,
+      password: password.value.modelValue }).then((res) => {
+        authStore.handleResponse(res.data, apiService);
+      if (authStore)
+        userStore.fetchCurrentUserBalance(authStore.getUser.id, apiService);
+        toHomeView();
+    }).catch((err) => {
+      console.error(err);
+    });
+  } else {
+    await authStore.gewisLdapLogin(username.value.modelValue, password.value.modelValue, apiService).then(() => {
+      if (authStore.getUser) userStore.fetchCurrentUserBalance(authStore.getUser.id, apiService);
+      toHomeView();
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+});
+
+//TODO fix the GEWIS login #32
 const loginViaGEWIS = () => {
   window.location.href = `https://gewis.nl/token/${import.meta.env.VITE_APP_GEWIS_TOKEN}`;
 };
@@ -88,13 +165,19 @@ const resetPassword = () => {
   router.push({ name: 'passwordreset' });
 };
 
+const toHomeView = () => {
+  router.push({ name: 'home' });
+};
+
 </script>
 
 <style scoped lang="scss">
+//TODO Fix the amount of css used related to #14 and #29
 form {
   display: flex;
   flex-direction: column;
 }
+
 h1 {
   color: black;
   max-width: 350px;
@@ -134,8 +217,56 @@ main {
   margin-right: 1rem;
 }
 
-label {
+.p-error {
+  display: block;
+  font-size: 12px;
+  text-align: left;
+  line-height:18px;
+  min-height: 25px;
+}
+
+.p-error > i {
+  font-size:12px;
+  margin-right: 3.6px;
+  line-height:12px;
+}
+
+#username {
+  width: 100%;
+  padding-top: 18px;
+  padding-left: 12px;
+  padding-bottom: 0;
+  height: 60px;
+}
+
+#password {
+  width: 100%;
+  padding-top: 18px;
+  padding-left: 12px;
+  padding-bottom: 0;
+  height: 60px;
+}
+
+.p-float-label label {
+  top: 30%;
+  margin-top: 0;
+  left: 12px;
+}
+
+.contains-text, .p-float-label input:focus ~ label,  .p-float-label label ~ input:focus {
+  margin-top: 0;
+  top: 8px!important;
+}
+
+#input-description {
   color: black;
+}
+
+.p-invalid {
+  background-color: #fef0f0;
+}
+
+.p-inputtext {
   margin-bottom: 0.5rem;
 }
 
