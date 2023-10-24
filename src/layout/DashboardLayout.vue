9b436554c95fdebe0a256114ac3733c47cdc7b0d
Victor Klomp <victor.klomp@gmail.com>
2023-10-24 20:26:41 +0200

    Whoops, this should be removed again

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -1,33 +1,13 @@
 <template>
-  <TopNavbar v-if="renderComponent"/>
+  <TopNavbar />
   <RouterView />
-  <CopyrightBanner v-if="renderComponent"/>
+  <CopyrightBanner />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import CopyrightBanner from "@/components/CopyrightBanner.vue";
 import TopNavbar from "@/components/TopNavbar.vue";
-import router from "@/router";
-import { computed, onMounted } from "vue";
-import { isAuthenticated, useAuthStore } from "@sudosos/sudosos-frontend-common";
-
-const renderComponent = computed(() => {
-  return router.currentRoute.value.name !== 'tos';
-});
-
-const authStore = useAuthStore();
-
-onMounted(async () => {
-  if(isAuthenticated && authStore.getUser.acceptedToS == 'NOT_ACCEPTED') {
-    toToSView();
-  }
-});
-
-const toToSView = () => {
-  router.push({ name: 'tos' });
-};
-
 </script>
 
 <style scoped>
