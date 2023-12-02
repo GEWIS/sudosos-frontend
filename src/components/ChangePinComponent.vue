4f70d58b26b76ff029a462f52acd570eee5cee51
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-12-02 19:43:09 +0100

    Added form validation to ChangePinComponent

--- src/components/ChangePinComponent.vue
+++ src/components/ChangePinComponent.vue
@@ -1,76 +1,65 @@
-<script setup lang="ts">
-import CardComponent from "@/components/CardComponent.vue";
-import PinComponent from "@/components/PinComponent.vue";
-import { useField } from "vee-validate";
-import apiService from "@/services/ApiService";
-import { useAuthStore } from "@sudosos/sudosos-frontend-common";
-import { useToast } from "primevue/usetoast";
-import { useI18n } from "vue-i18n";
-
-const authStore = useAuthStore();
-const toast = useToast();
-const { t } = useI18n();
-
-
-const { value: inputPin, errorMessage: inputPinError } = useField('inputPin', validatePin);
-const { value: confirmPin, errorMessage: confirmPinError } = useField('confirmPin', validateConfirmPin);
-
-
-
-//show warning message if pin is filled in and not 4 digits
-function validatePin(checkInputPin: string){
-  if (checkInputPin.length != 4 && !RegExp('\\d{4}').test(checkInputPin)) {
-    return t('profile.pinIncorrectLength');
-  }
-  return true;
-};
-
-//show warning message if conformation pin does not fit pin
-function validateConfirmPin(checkConfirmPin: string){
-  if (checkConfirmPin != inputPin.value) {
-    return t('profile.pinNotMatch');
-  }
-  return true;
-}
-
-
-function changePinCode() {
-  if (validatePin(inputPin.value) == true && validateConfirmPin(confirmPin.value) == true) {
-    authStore.updateUserPin(inputPin.value, apiService).then(() => {
-      //   Succes!
-      inputPin.value="";
-      confirmPin.value="";
-      toast.add({ severity: "success", summary: "success", detail: t('profile.pinUpdated')  });
-    }).catch((err) => {
-      // Error
-      console.error(err);
-    });
-  } else {
-    toast.add({ severity: "error", summary: "failed", detail: t('profile.pinIncorrect'), life: 3000 });
-  }
-}
-
-</script>
-
 <template>
   <CardComponent :header="$t('profile.pinChange')">
-    <div id="update-pin-form">
+    <form id="update-pin-form" @submit="changeUserPin">
       <div>
         <p>{{ $t('profile.pinNew')}}</p>
-        <PinComponent v-model="inputPin" />
-        <small class="warning">{{inputPinError || '&nbsp;'}}</small>
+        <InputText v-bind="pin" />
+        <small class="warning">{{errors.pin || '&nbsp;'}}</small>
       </div>
       <div>
         <p>{{ $t('profile.pinConfirm')}}</p>
-        <PinComponent v-model="confirmPin" />
-        <small class="warning">{{confirmPinError || '&nbsp;'}}</small>
+        <InputText v-bind="pinConfirm" />
+        <small class="warning">{{errors.pinConfirm || '&nbsp;'}}</small>
       </div>
       <div style="margin-top: 1rem">
-        <Button severity="danger" @click="changePinCode" :label="t('profile.pinChange')" />
+        <Button severity="danger" type="submit" :label="t('profile.pinChange')" />
       </div>
-    </div>
+    </form>
   </CardComponent>
 </template>
+
+<script setup lang="ts">
+import CardComponent from "@/components/CardComponent.vue";
+import { useForm } from "vee-validate";
+import apiService from "@/services/ApiService";
+import { useUserStore } from "@sudosos/sudosos-frontend-common";
+import { useToast } from "primevue/usetoast";
+import { useI18n } from "vue-i18n";
+import { editPinSchema } from "@/utils/validation-schema";
+import { handleError } from "@/utils/errorUtils";
+
+const userStore = useUserStore();
+const toast = useToast();
+const { t } = useI18n();
+const { defineComponentBinds, handleSubmit, errors } = useForm({
+  validationSchema: editPinSchema,
+});
+
+const pin = defineComponentBinds('pin', {});
+const pinConfirm = defineComponentBinds('pinConfirm', {});
+
+const changeUserPin = handleSubmit(async (values) => {
+  if (userStore.getCurrentUser.user) {
+    apiService.user.updateUserPin(
+      userStore.getCurrentUser.user.id,
+      { pin: values.pinConfirm })
+      .then(() => {
+        toast.add({
+          severity: "success",
+          summary: "Success",
+          detail: `${t('passwordUpdated')}`,
+          life: 3000,
+        });
+      })
+      .catch((err) => {
+        handleError(err, toast);
+      });
+  }
+});
+
+</script>
+
+
 
 <style scoped>
 
--- src/utils/validation-schema.ts
+++ src/utils/validation-schema.ts
@@ -25,3 +25,13 @@ export const editPasswordSchema = toTypedSchema(
     passwordConfirm: yup.string().required().oneOf([yup.ref('password')], 'Passwords must match'),
   })
 );
+
+export const editPinSchema = toTypedSchema(
+  yup.object({
+    pin: yup.string().required()
+      .matches(/^[0-9]+$/, "Must be only digits")
+      .min(4, 'Must be exactly 4 digits')
+      .max(4, 'Must be exactly 4 digits'),
+    pinConfirm: yup.string().required().oneOf([yup.ref('pin')], 'PINs must match'),
+  })
+);
