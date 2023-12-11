7a24d3f8f73cb1c91082a9a2dd7547a93c66b33a
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-12-11 19:04:57 +0100

    Added feature so people can see how much they have in fines

---
+++ src/views/FineView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-container">
+    <div class="page-title">{{ $t('home.Overview') }}</div>
+    <div class="content-wrapper">
+
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import apiService from "@/services/ApiService";
+
+const { t, locale } = useI18n();
+
+apiService.debtor
+</script>
+
+<style scoped>
+@import '../styles/BasePage.css';
+</style>
