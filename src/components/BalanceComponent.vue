06d3e07a99bd5f1a5a341c74d3c3f1bb25d4a01f
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-12-11 18:38:06 +0100

    Added feature so people can see how much they have in fines

--- src/components/BalanceComponent.vue
+++ src/components/BalanceComponent.vue
@@ -1,11 +1,12 @@
 <template>
   <CardComponent
-    :header="$t('c_currentBalance.balance')"
-    :action="showOption ? $t('c_currentBalance.Increase balance') : ''"
-    routerLink="balance"
+      :header="$t('c_currentBalance.balance')"
+      :action="showOption ? $t('c_currentBalance.Increase balance') : ''"
+      routerLink="balance"
   >
-    <div class="body">
+    <div class="flex flex-column justify-content-center">
       <h1>{{ displayBalance }}</h1>
+      <p class="text-center text-base font-semibold" v-if="fine">{{`Of which ${fine} is fines`}}</p>
     </div>
   </CardComponent>
 </template>
@@ -13,7 +14,7 @@
 <script setup lang="ts">
 import CardComponent from '@/components/CardComponent.vue';
 import { useUserStore } from '@sudosos/sudosos-frontend-common';
-import { computed, ref, watch } from 'vue';
+import { computed, ref, onMounted, Ref } from "vue";
 import type { UserResponse } from '@sudosos/sudosos-client';
 import apiService from '@/services/ApiService';
 import { formatPrice } from "@/utils/formatterUtils";
@@ -30,26 +31,33 @@ const props = defineProps({
 });
 
 const userStore = useUserStore();
-const balanceFromApi = ref<string | undefined>(undefined);
 
-// Watch for changes on props.user
-watch(() => props.user, async (newUser, oldUser) => {
-  if (newUser) {
-    const response = await apiService.balance.getBalanceId(newUser.id);
+const balanceFromApi = ref<string | undefined>(undefined);
+const fine: Ref<string | undefined> = ref();
+onMounted(async () => {
+  if (props.user) {
+    const response = await apiService.balance.getBalanceId(props.user.id);
     balanceFromApi.value = formatPrice(response.data.amount);
+    fine.value = response.data.fine ?
+      formatPrice(response.data.fine) : undefined;
   }
-}, { immediate: true });
+});
 
 const displayBalance = computed(() => {
   if (props.user) {
     return balanceFromApi.value;
   } else {
     const balanceInCents = userStore.getCurrentUser.balance;
+    if (userStore.getCurrentUser.balance && userStore.getCurrentUser.balance.fine){
+      fine.value = formatPrice(userStore.getCurrentUser.balance?.fine || { amount: 0, currency: 'EUR', precision: 2 });
+    }
     if (!balanceInCents) return undefined;
     const balanceInEuros = (balanceInCents.amount.amount / 100).toFixed(2);
     return `€${balanceInEuros}`;
   }
 });
+
+
 </script>
 
 <style scoped lang="scss">
@@ -58,5 +66,10 @@ h1 {
   text-align: center;
   font-family: Raleway, sans-serif !important;
   font-weight: 500;
+}
+
+p {
+  color: #d40000;
+  font-family: Raleway, sans-serif !important;
 }
 </style>
