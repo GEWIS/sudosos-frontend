32043867ed89f6f0b712557bdaeb9e72f127f023
Samuel <samuelincognito@gmail.com>
2023-12-12 20:11:07 +0100

    Removed css except for primevue styling

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,63 +1,63 @@
 <template>
   <div>
-    <main>
-      <img id="login-image" src="@/assets/img/bier.png" alt="logo" />
-      <h1>{{ $t('login.SudoSOS Login') }}</h1>
+    <main class="flex flex-column text-center mx-auto my-7 max-w-21rem">
+      <img class="max-h-9rem block mx-auto my-0" src="@/assets/img/bier.png" alt="logo"/>
+      <div class="text-900 text-5xl mt-0 mx-auto mb-2 w-full">{{ $t('login.SudoSOS Login') }}</div>
       <Button id="login-gewis-button" @click="loginViaGEWIS" severity="success">
-        <img id="gewis-branding" src="@/assets/img/gewis-branding.svg" alt="GEWIS" />
+        <img class="max-h-2rem py-1 mr-3" src="@/assets/img/gewis-branding.svg" alt="GEWIS"/>
         {{ $t('login.Login via GEWIS') }}
       </Button>
-      <hr />
-      <form id="login-form" @submit="loginHandler">
+      <hr class="mt-6 mb-4 max-w-21rem w-full"/>
+      <form class="flex flex-column" @submit="loginHandler">
         <label id="input-description" for="username">
           {{ $t('login.Username') }}
         </label>
         <span class="p-float-label with-error">
           <InputText
-            id="username"
-            type="text"
-            v-bind="username"
-            size="large"
-            name="username"
-            :class="{ 'p-invalid': loginForm.errors.value.username }"
+              type="text"
+              v-bind="username"
+              size="large"
+              name="username"
+              class="input-field"
+              :class="{ 'p-invalid': loginForm.errors.value.username }"
           />
           <label :class="{ 'contains-text': username.modelValue }" for="username"
-            >{{ $t('login.Username') }}
+          >{{ $t('login.Username') }}
           </label>
           <small class="p-error">
-            <i v-if="loginForm.errors.value.username" class="pi pi-exclamation-circle" />
+            <i v-if="loginForm.errors.value.username" class="pi pi-exclamation-circle"/>
             {{ loginForm.errors.value.username }}
           </small>
         </span>
-        <label id="input-description" for="password">
+        <label class="text-900" for="password">
           {{ $t('login.Password') }}
         </label>
         <span class="p-float-label with-error">
           <InputText
-            id="password"
-            type="password"
-            v-bind="password"
-            size="large"
-            name="password"
-            :class="{ 'p-invalid': loginForm.errors.value.password }"
+              type="password"
+              v-bind="password"
+              size="large"
+              name="password"
+              class="input-field"
+              :class="{ 'p-invalid': loginForm.errors.value.password }"
           />
           <label :class="{ 'contains-text': password.modelValue }" for="password"
-            >{{ $t('login.Password') }}
+          >{{ $t('login.Password') }}
           </label>
           <smal class="p-error">
-            <i v-if="loginForm.errors.value.password" class="pi pi-exclamation-circle" />
+            <i v-if="loginForm.errors.value.password" class="pi pi-exclamation-circle"/>
             {{ loginForm.errors.value.password }}
           </smal>
         </span>
         <Button type="submit" id="login-button">
           {{ $t('login.Login') }}
         </Button>
-        <div class="password-reset" @click="resetPassword">
+        <div class="text-900 underline" @click="resetPassword">
           {{ $t('login.Password reset') }}
         </div>
       </form>
     </main>
-    <CopyrightBanner />
+    <CopyrightBanner/>
   </div>
 </template>
 
@@ -83,10 +83,10 @@ const userStore = useUserStore();
 const toast = useToast();
 
 const schema = toTypedSchema(
-  yup.object({
-    username: yup.string().required('This is a required field.'),
-    password: yup.string().required('This is a required field.')
-  })
+    yup.object({
+      username: yup.string().required('This is a required field.'),
+      password: yup.string().required('This is a required field.')
+    })
 );
 const loginForm = useForm({
   validationSchema: schema
@@ -117,7 +117,7 @@ const loginHandler = loginForm.handleSubmit(async (values) => {
         accountMail: values.username,
         password: values.password
       }).then((res) => authStore.handleResponse(res.data, apiService));
-      if (authStore.getToS == 'ACCEPTED' && authStore.getUser ) {
+      if (authStore.getToS == 'ACCEPTED' && authStore.getUser) {
         await userStore.fetchCurrentUserBalance(authStore.getUser.id, apiService);
       } else {
         await router.replace({ path: '/error' });
@@ -152,35 +152,6 @@ const toHomeView = () => {
 
 <style scoped lang="scss">
 //TODO Fix the amount of css used related to #14 and #29
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-h1 {
-  color: black;
-  max-width: 350px;
-  width: 100%;
-  margin: 0 auto;
-  font-size: 2.5rem;
-  margin-bottom: 1.5rem;
-}
-
-#login-image {
-  max-height: 150px;
-
-  display: block;
-  margin: 0 auto;
-}
-
-main {
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  max-width: 350px;
-  margin: 4rem auto;
-}
-
 .p-button {
   margin: 1rem auto;
   max-width: 350px;
@@ -189,11 +160,6 @@ main {
   display: flex;
   align-items: center;
   justify-content: center;
-}
-
-#gewis-branding {
-  max-height: 24px;
-  margin-right: 1rem;
 }
 
 .p-error {
@@ -210,15 +176,7 @@ main {
   line-height: 12px;
 }
 
-#username {
-  width: 100%;
-  padding-top: 18px;
-  padding-left: 12px;
-  padding-bottom: 0;
-  height: 60px;
-}
-
-#password {
+.input-field {
   width: 100%;
   padding-top: 18px;
   padding-left: 12px;
@@ -239,10 +197,6 @@ main {
   top: 8px !important;
 }
 
-#input-description {
-  color: black;
-}
-
 .p-invalid {
   background-color: #fef0f0;
 }
@@ -251,18 +205,7 @@ main {
   margin-bottom: 0.5rem;
 }
 
-hr {
-  margin-top: 3rem;
-  margin-bottom: 1.5rem;
-  max-width: 350px;
-  width: 100%;
-}
-
 .p-inputtext {
   margin-bottom: 0.5rem;
-}
-.password-reset {
-  color: black;
-  text-decoration-line: underline;
 }
 </style>
