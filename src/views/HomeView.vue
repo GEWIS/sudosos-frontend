8ca1d52157a9ea5c1c0f10af682a525373845ade
Samuel <samuelincognito@gmail.com>
2023-12-12 19:51:10 +0100

    Fixed HomeView.vue en App.vue to use primeflex

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="page-container">
     <div class="page-title">{{ $t('home.Overview') }}</div>
-    <div class="content-wrapper">
-      <BalanceComponent class="balance-component" :showOption="true"/>
+    <div class="content-wrapper gap-5">
+      <BalanceComponent :showOption="true"/>
       <TransactionsTableComponent
-          class="transactions-table"
           :header="$t('c_recentTransactionsTable.recent transactions')"
           :action="$t('c_recentTransactionsTable.all transactions')"
       />
@@ -20,12 +19,4 @@ import TransactionsTableComponent from "@/components/TransactionsTableComponent.
 
 <style scoped lang="scss">
 @import "../styles/BasePage.css";
-
-.balance-component {
-  margin-right: 15px;
-}
-
-.transactions-table {
-  margin-left: 15px;
-}
 </style>
