445b944d09a78100913d9c917fa1439e11c25320
Samuel <samuelincognito@gmail.com>
2023-12-24 23:09:08 +0100

    Fixed type errors.

--- src/components/UserInfoComponent.vue
+++ src/components/UserInfoComponent.vue
@@ -1,27 +1,32 @@
 <template>
-  <CardComponent
-      :header="$t('c_userInfo.quick')"
-  >
+  <CardComponent :header="$t('c_userInfo.quick')">
     <div class="body">
-      <p class="text-gray-700">Show this at the bar so we can help you faster. </p>
-      <h1 class="text-center"> {{ props.user.gewisId ? props.user.gewisId : `E${props.user.id}` }} <br/>
-        {{ props.user.firstName }} {{ props.user.lastName }} </h1>
+      <p class="text-gray-700">Show this at the bar so we can help you faster.</p>
+      <h1 class="text-center">
+        {{ isGewisUser(props.user) ? props.user.gewisId : `E${props.user.id}` }} <br/>
+        {{ props.user.firstName }} {{ props.user.lastName }}
+      </h1>
     </div>
   </CardComponent>
 </template>
 
 <script setup lang="ts">
-import type {GewisUserResponse, UserResponse} from "@sudosos/sudosos-client";
+import type { GewisUserResponse, UserResponse } from "@sudosos/sudosos-client";
 import CardComponent from "@/components/CardComponent.vue";
 
 const props = defineProps({
   user: {
     type: Object as () => UserResponse | GewisUserResponse,
-    required: false,
+    required: true,
   },
 });
+
+// Type guard
+function isGewisUser(user: UserResponse | GewisUserResponse): user is GewisUserResponse {
+  return (user as GewisUserResponse).gewisId !== undefined;
+}
 </script>
 
 <style scoped lang="scss">
-
+// Your styles here
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,15 @@
   <div class="page-container flex flex-column">
     <div class="page-title">{{ $t('home.Overview') }}</div>
     <div class="content-wrapper gap-5 flex md:flex-row flex-column">
-      <UserInfoComponent :user="userStore.getCurrentUser.user" class="xl:hidden lg:hidden"/>
+      <UserInfoComponent :user="userStore.getCurrentUser.user as GewisUserResponse | UserResponse" class="xl:hidden lg:hidden"/>
       <BalanceComponent :showOption="true"/>
       <MutationsTableComponent
-        :callback-function="getUserMutations"
-        :header="$t('c_recentTransactionsTable.recent transactions')"
-        :action="$t('c_recentTransactionsTable.all transactions')"
-        :paginator="false"
-        :modal="false"
-        routerLink="transaction-view"
+          :callback-function="getUserMutations"
+          :header="$t('c_recentTransactionsTable.recent transactions')"
+          :action="$t('c_recentTransactionsTable.all transactions')"
+          :paginator="false"
+          :modal="false"
+          routerLink="transaction-view"
       />
     </div>
   </div>
@@ -21,24 +21,27 @@ import BalanceComponent from '@/components/BalanceComponent.vue';
 import MutationsTableComponent from '@/components/Mutations/MutationsTableComponent.vue';
 import { useAuthStore, useUserStore } from '@sudosos/sudosos-frontend-common';
 import apiService from '@/services/ApiService';
-import type { PaginatedFinancialMutationResponse } from '@sudosos/sudosos-client';
+import type { GewisUserResponse, PaginatedFinancialMutationResponse, UserResponse } from '@sudosos/sudosos-client';
 import router from "@/router";
 import { handleError } from "@/utils/errorUtils";
 import { useToast } from "primevue/usetoast";
 import UserInfoComponent from "@/components/UserInfoComponent.vue";
+import { storeToRefs } from "pinia";
 
 const authStore = useAuthStore();
 const userStore = useUserStore();
 const toast = useToast();
 
-const getUserMutations = async (take: number, skip: number) :
-  Promise<PaginatedFinancialMutationResponse | undefined> => {
+const userRefs = storeToRefs(userStore);
+
+const getUserMutations = async (take: number, skip: number):
+    Promise<PaginatedFinancialMutationResponse | undefined> => {
   if (!authStore.getUser) {
     await router.replace({ path: '/error' });
     return;
   }
   await userStore.fetchUsersFinancialMutations(authStore.getUser.id, apiService, take, skip)
-    .catch((err) => handleError(err, toast));
+      .catch((err) => handleError(err, toast));
   return userStore.getCurrentUser.financialMutations;
 };
 </script>
