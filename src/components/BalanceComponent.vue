c0307688f4a7583a8c6cdf802658773d352a7dd7
Samuel <samuelincognito@gmail.com>
2024-01-08 12:03:15 +0100

    Refactored the BalanceComponent.vue, also correctly handles the use case where fine > balance.

--- src/components/BalanceComponent.vue
+++ src/components/BalanceComponent.vue
@@ -5,19 +5,22 @@
       routerLink="balance"
   >
     <div class="flex flex-column justify-content-center">
-      <h1>{{ displayBalance }}</h1>
-      <p class="text-center text-base font-semibold" v-if="fine">{{`Of which ${fine} is fines`}}</p>
+      <h1 class="text-center font-medium text-6xl">{{ displayBalance }}</h1>
+      <p class="text-center text-base font-semibold text-red-500" v-if="userBalance && userBalance.fine">
+        {{ isAllFine ? $t('c_currentBalance.allIsFines') : $t('c_currentBalance.someIsFines', { fine: displayFine }) }}
+      </p>
     </div>
   </CardComponent>
 </template>
 
 <script setup lang="ts">
 import CardComponent from '@/components/CardComponent.vue';
-import { useUserStore } from '@sudosos/sudosos-frontend-common';
-import { computed, ref, onMounted, Ref } from "vue";
-import type { UserResponse } from '@sudosos/sudosos-client';
+import { useAuthStore, useUserStore } from '@sudosos/sudosos-frontend-common';
+import { computed, ref, onMounted, Ref, watch } from "vue";
+import type { BalanceResponse, UserResponse } from '@sudosos/sudosos-client';
 import apiService from '@/services/ApiService';
 import { formatPrice } from "@/utils/formatterUtils";
+import { storeToRefs } from "pinia";
 
 const props = defineProps({
   user: {
@@ -31,45 +34,47 @@ const props = defineProps({
 });
 
 const userStore = useUserStore();
-
-const balanceFromApi = ref<string | undefined>(undefined);
-const fine: Ref<string | undefined> = ref();
-onMounted(async () => {
+const userBalance: Ref<BalanceResponse | null> = ref(null);
+const { current } = storeToRefs(userStore);
+const updateUserBalance = async () => {
   if (props.user) {
     const response = await apiService.balance.getBalanceId(props.user.id);
-    balanceFromApi.value = formatPrice(response.data.amount);
-    fine.value = response.data.fine ?
-      formatPrice(response.data.fine) : undefined;
+    userBalance.value = response.data;
+  } else {
+    // Force refresh balance, since people tend to refresh pages like this to ensure an up to date balance.
+    const auth = useAuthStore();
+    await userStore.fetchCurrentUserBalance(auth.getUser.id, apiService);
+    userBalance.value = userStore.getCurrentUser.balance;
   }
+};
+
+onMounted(updateUserBalance);
+
+watch(() => props.user, () => {
+  updateUserBalance();
+});
+
+watch(current, () => {
+  updateUserBalance();
+});
+
+
+const isAllFine = computed(() => {
+  if (!userBalance.value?.fine) return false;
+  return userBalance.value.fine.amount >= userBalance.value?.amount.amount;
+});
+
+const displayFine = computed(() => {
+  if (!userBalance.value?.fine) return undefined;
+  return formatPrice(userBalance.value.fine || { amount: 0, currency: 'EUR', precision: 2 });
 });
 
 const displayBalance = computed(() => {
-  if (props.user) {
-    return balanceFromApi.value;
-  } else {
-    const balanceInCents = userStore.getCurrentUser.balance;
-    if (userStore.getCurrentUser.balance && userStore.getCurrentUser.balance.fine){
-      fine.value = formatPrice(userStore.getCurrentUser.balance?.fine || { amount: 0, currency: 'EUR', precision: 2 });
-    }
-    if (!balanceInCents) return undefined;
-    const balanceInEuros = (balanceInCents.amount.amount / 100).toFixed(2);
-    return `€${balanceInEuros}`;
-  }
+  if (!userBalance.value?.amount.amount) return undefined;
+  return formatPrice(userBalance.value.amount);
 });
-
 
 </script>
 
 <style scoped lang="scss">
-h1 {
-  font-size: 50px;
-  text-align: center;
-  font-family: Raleway, sans-serif !important;
-  font-weight: 500;
-}
-
-p {
-  color: #d40000;
-  font-family: Raleway, sans-serif !important;
-}
 </style>
