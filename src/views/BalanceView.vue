89fb441031b7114899fb140628ecbbedd3aa1ac3
CodeNamedRobin <robinvandijke2002@gmail.com>
2024-04-04 00:32:25 +0200

    Fixed issue where people could not top up to exactly 0 balance.

--- src/views/BalanceView.vue
+++ src/views/BalanceView.vue
@@ -16,7 +16,7 @@
               locale="nl-NL"
             />
           </div>
-          <p v-if="!isUndefined(amountValue) && amountValue < 10" class="font-bold text-red-500">
+          <p v-if="!isUndefined(amountValue) && amountValue < 10 && amountValue != -1 * userBalance" class="font-bold text-red-500">
             {{ $t('balance.minPayment') }}
           </p>
           <br v-else />
@@ -25,7 +25,7 @@
         <div class="flex justify-content-end">
         <Button
           @click="showDialog"
-          :disabled="isUndefined(amountValue) || amountValue < 10"
+          :disabled="isUndefined(amountValue) || amountValue < 10 && amountValue != -1 * userBalance"
         >
           {{ $t('balance.Start payment') }}
         </Button>
@@ -37,9 +37,12 @@
 
 <script setup lang="ts">
 import CardComponent from "@/components/CardComponent.vue";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import TopupModal from "@/components/TopupModalComponent.vue";
 import { isUndefined } from "lodash";
+import { useUserStore } from "@sudosos/sudosos-frontend-common";
+import router from "@/router";
+import apiService from "@/services/ApiService";
 
 // Define the 'visible' ref variable to control dialog visibility
 const visible = ref(false);
@@ -49,6 +52,24 @@ const amountValue = ref(undefined);
 const showDialog = () => {
   visible.value = true;
 };
+
+const userBalance = ref();
+
+const userStore = useUserStore();
+
+onMounted(async () => {
+  if (!userStore.getCurrentUser.user){
+    await router.replace({ path: '/error' });
+    return;
+  }
+  await userStore.fetchCurrentUserBalance(userStore.getCurrentUser.user.id, apiService);
+  if (!userStore.getCurrentUser.balance?.amount) {
+    await router.replace({ path: '/error' });
+    return;
+  }
+  userBalance.value = userStore.getCurrentUser.balance.amount.amount / 100;
+  console.warn(userBalance.value);
+});
 </script>
 
 <style scoped>
