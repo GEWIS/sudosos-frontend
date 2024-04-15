1f5378744d69f8b74ef7105d4a514519db70cd52
CodeNamedRobin <robinvandijke2002@gmail.com>
2024-04-15 23:53:37 +0200

    Switched UserChangeInfoComponent to UserInfoComponent

--- src/components/UserInfoComponent.vue
+++ src/components/UserInfoComponent.vue
@@ -1,13 +1,11 @@
 <template>
-  <CardComponent :header="$t('c_userInfo.quick')">
-    <div class="body">
-      <p class="text-gray-700">{{$t('quickOverview.quickOverviewMessage')}}</p>
-      <h1 class="text-center">
-        {{ isGewisUser(props.user) ? props.user.gewisId : `E${props.user.id}` }} <br/>
-        {{ props.user.firstName }} {{ props.user.lastName }}
-      </h1>
-    </div>
-  </CardComponent>
+    <CardComponent :header="$t('c_userInfo.quick')" >
+        <p class="text-gray-700">{{ $t("quickOverview.quickOverviewMessage") }}</p>
+        <h1 class="text-center">
+          {{ isGewisUser(props.user) ? props.user.gewisId : `E${props.user.id}` }} <br />
+          {{ props.user.firstName }} {{ props.user.lastName }}
+        </h1>
+    </CardComponent>
 </template>
 
 <script setup lang="ts">
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -2,18 +2,19 @@
   <div class="page-container">
     <div class="page-title">{{ $t('profile.myProfile')}}</div>
     <div class="grid">
-      <div class="col-12 md:col-6">
+      <div class="col-6 md:col-6">
+        <UserInfoComponent :user="current.user as UserResponse"/>
+      </div>
+      <div class="col-6 md:col-6">
           <ChangePinComponent />
       </div>
-      <div class="col-12 md:col-6">
+      <div class="col-6 md:col-6">
         <ChangePasswordComponent />
       </div>
-      <div class="col-12 md:col-6">
+      <div class="col-6 md:col-6">
         <ChangeApiKeyComponent />
       </div>
-      <div class="col-12 md:col-6">
-        <ChangeInfoComponent />
-      </div>
+
     </div>
   </div>
 </template>
@@ -23,6 +24,14 @@ import ChangePasswordComponent from "@/components/ChangePasswordComponent.vue";
 import ChangePinComponent from "@/components/ChangePinComponent.vue";
 import ChangeApiKeyComponent from "@/components/ChangeApiKeyComponent.vue";
 import ChangeInfoComponent from "@/components/ChangeInfoComponent.vue";
+import { useUserStore } from "@sudosos/sudosos-frontend-common";
+import { storeToRefs } from "pinia";
+import UserInfoComponent from "@/components/UserInfoComponent.vue";
+import type { UserResponse } from "@sudosos/sudosos-client";
+
+const userStore = useUserStore();
+
+const { current } = storeToRefs(userStore);
 </script>
 
 <style scoped lang="scss">
