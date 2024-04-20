8c5cfcd5f425c2cc5c19118220b5e1a9a2218ae0
Samuel <samuelincognito@gmail.com>
2024-04-20 11:57:28 +0200

    feat: improved dialog layout and styling

--- apps/dashboard/src/components/ProductDialogComponent.vue
+++ apps/dashboard/src/components/ProductDialogComponent.vue
@@ -1,58 +1,59 @@
 <template>
-  <Dialog v-model:visible="visible" modal :header="$t('c_productInfoModal.title')" class="flex w-9">
-    <!-- Row for Name -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Name") }}</h4>
-      <p class="my-0">{{ product.name }}</p>
-    </div>
+  <Dialog v-model:visible="visible" ref="dialog" modal :draggable="false" :header="product.name" :pt="{content: 'p-0'}" @show="addListenerOnDialogueOverlay(dialog)">
+    <div class="flex gap-5 overflow-hidden">
+      <!-- Row for Picture -->
+      <div class="picture-container">
+        <img class="product-image" :src="getProductImageSrc(product)" :alt="product.name">
+      </div>
 
-    <!-- Row for Added by -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Added by") }}</h4>
-      <p class="my-0">{{ product.owner.firstName + ' ' + product.owner.lastName }}</p>
-    </div>
+      <div class="flex flex-column w-25rem justify-content-between pr-5 py-5">
+        <div class="flex flex-column">
+          <!-- Row for Name -->
+          <div class="flex flex-row justify-content-between">
+            <h4 class="my-0">{{ $t("c_productInfoModal.Name") }}</h4>
+            <p class="my-0">{{ product.name }}</p>
+          </div>
 
-    <!-- Row for Added on -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Added on") }}</h4>
-      <p class="my-0">{{ formatDateTime(new Date(product.createdAt ? product.createdAt.toString() : '')) }}</p>
-    </div>
+          <!-- Row for Added by -->
+          <div class="flex flex-row justify-content-between">
+            <h4 class="my-0">Owner</h4>
+            <p class="my-0">{{ product.owner.firstName + ' ' + product.owner.lastName }}</p>
+          </div>
 
-    <!-- Row for Updated on -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Updated on") }}</h4>
-      <p class="my-0">{{ formatDateTime(new Date(product.updatedAt ? product.updatedAt.toString() : '')) }}</p>
-    </div>
+          <!-- Row for Added on -->
+          <div class="flex flex-row justify-content-between">
+            <h4 class="my-0">{{ $t("c_productInfoModal.Added on") }}</h4>
+            <p class="my-0">{{ formatDateTime(new Date(product.createdAt ? product.createdAt.toString() : '')) }}</p>
+          </div>
 
-    <!-- Row for Price -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Price") }}</h4>
-      <p class="my-0">{{ formatPrice(product.priceInclVat) }}</p>
-    </div>
+          <!-- Row for Updated on -->
+          <div class="flex flex-row justify-content-between">
+            <h4 class="my-0">{{ $t("c_productInfoModal.Updated on") }}</h4>
+            <p class="my-0">{{ formatDateTime(new Date(product.updatedAt ? product.updatedAt.toString() : '')) }}</p>
+          </div>
 
-    <!-- Row for Category -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Category") }}</h4>
-      <p class="my-0">{{ product.category.name }}</p>
-    </div>
+          <!-- Row for Price -->
+          <div class="flex flex-row justify-content-between">
+            <h4 class="my-0">{{ $t("c_productInfoModal.Price") }}</h4>
+            <p class="my-0">{{ formatPrice(product.priceInclVat) }}</p>
+          </div>
 
-    <!-- Row for Alcohol percentage -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Alcohol percentage") }}</h4>
-      <p class="my-0">{{ product.alcoholPercentage + '%' }}</p>
-    </div>
+          <!-- Row for Category -->
+          <div class="flex flex-row justify-content-between">
+            <h4 class="my-0">{{ $t("c_productInfoModal.Category") }}</h4>
+            <p class="my-0">{{ product.category.name }}</p>
+          </div>
 
-    <!-- Row for Picture -->
-    <div class="flex flex-row justify-content-between">
-      <h4 class="my-0">{{ $t("c_productInfoModal.Picture") }}</h4>
-      <img class="max-w-10rem max-h-10rem h-full" :src="getProductImageSrc(product)" :alt="product.name">
-
-    </div>
-    <br>
-    <div class="flex justify-content-end">
-      <Button @click="handleDeleteProduct">
-        {{ $t("c_productInfoModal.delete") }}
-      </Button>
+          <!-- Row for Alcohol percentage -->
+          <div class="flex flex-row justify-content-between">
+            <h4 class="my-0">{{ $t("c_productInfoModal.Alcohol percentage") }}</h4>
+            <p class="my-0">{{ product.alcoholPercentage + '%' }}</p>
+          </div>
+        </div>
+        <Button @click="handleDeleteProduct" class="w-auto ml-auto">
+          Delete from container
+        </Button>
+      </div>
     </div>
   </Dialog>
 </template>
@@ -60,7 +61,7 @@
 <script setup lang="ts">
 import type { ContainerWithProductsResponse, ProductResponse } from "@sudosos/sudosos-client";
 import Dialog from 'primevue/dialog';
-import { ref } from "vue";
+import { type Ref, ref } from "vue";
 import { getProductImageSrc } from "@/utils/imageUtils";
 import { formatDateTime, formatPrice } from "@/utils/formatterUtils";
 import apiService from "@/services/ApiService";
@@ -68,6 +69,9 @@ import { useToast } from "primevue/usetoast";
 import { useI18n } from "vue-i18n";
 import { handleError } from "@/utils/errorUtils";
 import { useRouter } from "vue-router";
+import { addListenerOnDialogueOverlay } from "@/utils/dialogUtil";
+
+const dialog: Ref<null|any> = ref(null);
 
 const router = useRouter();
 const toast = useToast();
@@ -85,12 +89,12 @@ const visible = ref(false);
 const { t } = useI18n();
 const handleDeleteProduct = async () => {
   const newProducts = props.container.products.filter(product => product.id !== props.product.id)
-    .map(product => product.id);
+      .map(product => product.id);
   await apiService.container.updateContainer(props.container.id, {
     name: props.container.name,
     products: newProducts,
     public: props.container.public || false,
-    }).then(() => {
+  }).then(() => {
         toast.add({
           severity: 'success',
           summary: t('successMessages.success'),
@@ -99,10 +103,29 @@ const handleDeleteProduct = async () => {
         });
         router.go(0);
       }
-    ).catch((error) => handleError(error, toast));
+  ).catch((error) => handleError(error, toast));
 };
 
 </script>
 
 <style scoped lang="scss">
+.alt-dialog-content {
+  padding: 0 !important;
+}
+
+.picture-container {
+  width: 21rem; /* 336px if 1rem = 16px */
+  height: 21rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff; /* Ensuring background is white */
+}
+.product-image {
+  max-width: 100%;
+  max-height: 100%;
+  width: auto; /* New line: Ensures width is auto-adjusted */
+  height: 100%; /* New line: Ensures height is auto-adjusted */
+  object-fit: cover; /* Changes from contain to cover to ensure full square area is used */
+}
 </style>
