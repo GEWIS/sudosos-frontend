05baa59fb6a7e30686fd4df1b79b6e179160ed8c
Samuel <samuelincognito@gmail.com>
2024-05-22 21:45:47 +0200

    feat: updated stores to a more function state

--- apps/dashboard/src/stores/container.store.ts
+++ apps/dashboard/src/stores/container.store.ts
@@ -1,33 +1,142 @@
 import { defineStore } from "pinia";
-import type { ContainerWithProductsResponse } from "@sudosos/sudosos-client";
+import type {
+    ContainerResponse,
+    ContainerWithProductsResponse,
+    ProductResponse,
+    UpdateContainerRequest
+} from "@sudosos/sudosos-client";
+import { fetchAllPages } from '@sudosos/sudosos-frontend-common';
 import ApiService from "@/services/ApiService";
+
+export type ContainerInStore = ContainerWithProductsResponse | ContainerResponse;
+
+export function isContainerWithProductsResponse(container: ContainerInStore): container is ContainerWithProductsResponse {
+    return (container as ContainerWithProductsResponse).products !== undefined;
+}
 
 export const useContainerStore = defineStore('container', {
     state: () => ({
-        container: null as ContainerWithProductsResponse | null,
+        containers: {} as Record<number, ContainerInStore>,
     }),
     getters: {
-        getContainer(): ContainerWithProductsResponse | null {
-            return this.container;
+        getContainer: (state) => (c: ContainerResponse): ContainerInStore | null => {
+            return state.containers[c.id] || null;
+        },
+        getAllContainers(state): Record<number, ContainerInStore> {
+            return state.containers;
+        },
+        getPublicContainers(state): Record<number, ContainerInStore> {
+            return Object.fromEntries(
+              Object.entries(state.containers).filter(([_, container]) => container.public)
+            );
+        },
+        getUsersContainers(state): Record<number, ContainerInStore> {
+            return Object.fromEntries(
+              Object.entries(state.containers).filter(([_, container]) => container.public === false)
+            );
         }
     },
     actions: {
-        async fetchContainer(id: number){
-
+        async fetchAllIfEmpty() {
+          if (Object.keys(this.containers).length === 0) {
+            await this.fetchAllContainers();
+          }
+        },
+        async fetchContainer(id: number) {
             const resp = await ApiService.container.getSingleContainer(id);
-            this.container = resp.data;
+            this.containers[id] = resp.data;
         },
-        async getPublicContainers(){
-            const response = await ApiService.container.getPublicContainers();
-            return response.data;
+        async fetchAllContainers() {
+            const containersArray = await fetchAllPages<ContainerWithProductsResponse>(
+              0,
+              Number.MAX_SAFE_INTEGER,
+              // @ts-ignore
+              (take, skip) => ApiService.container.getAllContainers(take, skip)
+            );
+            containersArray.forEach((container: ContainerWithProductsResponse) => {
+                this.containers[container.id] = container;
+            });
         },
-        async getUsersContainers(id: number) {
-            const response = await ApiService.user.getUsersContainers(id);
-            return response.data;
+        async addProductToContainer(c: ContainerResponse, product: ProductResponse) {
+            const container = this.containers[c.id];
+            if (!container) {
+                throw new Error(`Container with id ${c.id} not found`);
+            }
+
+            if (!isContainerWithProductsResponse(container)) {
+                throw new Error(`Container with id ${c.id} does not have products loaded`);
+            }
+
+            const updateContainerReq: UpdateContainerRequest = {
+                name: container.name,
+                products: [product.id, ...container.products.map(e => e.id)],
+                public: container.public || false,
+            };
+
+            container.products.push(product);
+            return ApiService.container.updateContainer(container.id, updateContainerReq);
         },
-        async createEmptyContainer(name: string, isPublic: boolean, ownerId: number){
-            return await ApiService.container.createContainer(
-                { name: name, products: [], public: isPublic, ownerId: ownerId });
+        async deleteProductFromContainer(c: ContainerResponse, product: ProductResponse) {
+            const container = this.containers[c.id];
+            if (!container) {
+                throw new Error(`Container with id ${c.id} not found`);
+            }
+
+            if (!isContainerWithProductsResponse(container)) {
+                throw new Error(`Container with id ${c.id} does not have products loaded`);
+            }
+
+            const updateContainerReq: UpdateContainerRequest = {
+                name: container.name,
+                products: container.products.filter(e => e.id !== product.id).map(e => e.id),
+                public: container.public || false,
+            };
+
+            container.products = container.products.filter(e => e.id !== product.id);
+            return ApiService.container.updateContainer(container.id, updateContainerReq);
+        },
+        async handleProductUpdate(productResponse: ProductResponse) {
+            const containers = Object.values(this.containers).filter(isContainerWithProductsResponse);
+            for (const container of containers) {
+                const productIndex = container.products.findIndex(p => p.id === productResponse.id);
+                if (productIndex !== -1) {
+                    container.products[productIndex] = productResponse;
+                }
+            }
+        },
+        async createEmptyContainer(name: string, isPublic: boolean, ownerId: number) {
+            const response = await ApiService.container.createContainer(
+              { name: name, products: [], public: isPublic, ownerId: ownerId });
+            const container = response.data;
+
+            this.containers[container.id] = container;
+
+            return container;
+        },
+        async getContainerWithProducts(c: ContainerResponse, force: boolean = false):
+          Promise<ContainerWithProductsResponse | null> {
+            let container = this.containers[c.id];
+
+            if (!container) {
+                // Fetch container if not already in state
+                const resp = await ApiService.container.getSingleContainer(c.id);
+                container = resp.data;
+                this.containers[c.id] = container;
+            }
+
+            if (force || (container && (!isContainerWithProductsResponse(container)))) {
+                // Fetch products for the container if not already present or if force is true
+                const products = await fetchAllPages<ProductResponse>(
+                  0,
+                  Number.MAX_SAFE_INTEGER,
+                  // @ts-ignore
+                  (take, skip) => ApiService.container.getProductsContainer(c.id, take, skip)
+                );
+                // Ensure reactivity by creating a new container object
+                this.containers[c.id] = { ...container, products };
+            }
+
+            return this.containers[c.id] as ContainerWithProductsResponse;
         }
     }
 });
