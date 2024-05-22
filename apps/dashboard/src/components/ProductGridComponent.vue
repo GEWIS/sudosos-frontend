8bc9c105e08654803226c42324a7c7cd3b1ead80
Samuel <samuelincognito@gmail.com>
2024-05-22 21:52:09 +0200

    feat(manage-products): enhanced styling

--- apps/dashboard/src/components/ProductGridComponent.vue
+++ apps/dashboard/src/components/ProductGridComponent.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="flex flex-row flex-wrap w-full gap-1 w-full">
+  <div class="flex flex-row flex-wrap w-full gap-2 w-full justify-space-between">
     <!-- Loop through each product and render a card for each -->
-
-    <div v-for="product in products" :key="product.id" class="card">
+    <div v-for="product in container.products" :key="product.id" class="card">
       <ProductComponent :container="container" :product="product" />
     </div>
     <Button class="flex flex-column h-10rem w-8rem border-300
     border-1 border-round-sm overflow-hidden" :outlined="true" @click="visible = true">
       <i class="pi pi-plus text-5xl my-auto product-card"></i>
     </Button>
-    <ContainerAssignProductDialog v-model:visible="visible" v-model:products="products" :container="props.container"/>
+    <ContainerAssignProductDialog v-model:visible="visible" :container="props.container"/>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { ContainerWithProductsResponse, ProductResponse } from "@sudosos/sudosos-client";
+import type { ContainerWithProductsResponse } from "@sudosos/sudosos-client";
 import ProductComponent from "@/components/ProductComponent.vue";
 import ContainerAssignProductDialog from "@/components/ContainerAssignProductDialog.vue";
 import { type Ref, ref, type PropType } from "vue";
@@ -26,7 +25,6 @@ const props = defineProps({
     required: true
   }
 });
-const products: Ref<ProductResponse[]> = ref(props.container.products);
 
 </script>
 
