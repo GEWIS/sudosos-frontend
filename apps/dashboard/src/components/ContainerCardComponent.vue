04dcf0e6e6ad779b2c53f1811df1b601366bb5d3
Samuel <samuelincognito@gmail.com>
2024-05-22 21:49:05 +0200

    feat(manage-products): ContainerCardComponent.vue now lazy loads (and caches) container products

--- apps/dashboard/src/components/ContainerCardComponent.vue
+++ apps/dashboard/src/components/ContainerCardComponent.vue
@@ -1,38 +1,42 @@
 <template>
   <CardComponent :header="$t('containersOverview.Containers')" class="p-0">
-<!--    <TabView :active-index=1 class="hidden md:block" :scrollable="true">-->
-<!--      <TabPanel v-for="container in containers" :key="container.id" :header="container.name">-->
-<!--        <ProductGridComponent :container="container" :products="container.products"/>-->
-<!--      </TabPanel>-->
-<!--      <TabPanel>-->
-<!--        <template #header>-->
-<!--            <span class="p-tabview-title createContainerButton">{{ $t('containersOverview.newContainer') }}</span>-->
-<!--        </template>-->
-<!--      </TabPanel>-->
-<!--    </TabView>-->
-    <Accordion :activeIndex="0" class="block w-full" :multiple="true">
+    <!--    <TabView :active-index=1 class="hidden md:block" :scrollable="true">-->
+    <!--      <TabPanel v-for="container in containers" :key="container.id" :header="container.name">-->
+    <!--        <ProductGridComponent :container="container" :products="container.products"/>-->
+    <!--      </TabPanel>-->
+    <!--      <TabPanel>-->
+    <!--        <template #header>-->
+    <!--            <span class="p-tabview-title createContainerButton">{{ $t('containersOverview.newContainer') }}</span>-->
+    <!--        </template>-->
+    <!--      </TabPanel>-->
+    <!--    </TabView>-->
+    <Accordion :activeIndex="0" class="block w-full" :multiple="true"
+               @tab-open="(event: AccordionTabOpenEvent) => onTabOpen(event)">
       <AccordionTab v-for="container in containers" :key="container.id" :header="container.name">
-        <ProductGridComponent :container="container" :products="container.products"/>
+        <ProductGridComponent :container="container"/>
       </AccordionTab>
     </Accordion>
   </CardComponent>
 
 </template>
 <script setup lang="ts">
-import type { ContainerWithProductsResponse } from "@sudosos/sudosos-client";
 import ProductGridComponent from "@/components/ProductGridComponent.vue";
-import TabView from 'primevue/tabview';
-import TabPanel from 'primevue/tabpanel';
 import CardComponent from "./CardComponent.vue";
-import Accordion from "primevue/accordion";
+import Accordion, { type AccordionTabOpenEvent } from "primevue/accordion";
 import AccordionTab from "primevue/accordiontab";
+import { type ContainerInStore, useContainerStore } from "@/stores/container.store";
 
-defineProps({
+const props = defineProps({
   containers: {
-    type: Array<ContainerWithProductsResponse>,
+    type: Array<ContainerInStore>,
     required: true,
   },
 });
+
+const containerStore = useContainerStore();
+const onTabOpen = async (event: AccordionTabOpenEvent) => {
+  await containerStore.getContainerWithProducts(props.containers[event.index]);
+};
 </script>
 <style scoped lang="scss">
 
