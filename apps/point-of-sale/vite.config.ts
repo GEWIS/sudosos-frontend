d83d4f3999fa7413b6e31d0369f8869b2e21a160
Victor Klomp <victor.klomp@gmail.com>
2024-07-24 22:25:35 +0200

    ci(vite-config): Proxy api request through vite server

--- apps/point-of-sale/vite.config.ts
+++ apps/point-of-sale/vite.config.ts
@@ -1,31 +1,42 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import useGitInfo from "./vite.config.plugin-git-info";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
               @import "./src/scss/styles.scss";
             `
+        }
       }
+    },
+    server: {
+      port: 5174,
+      proxy: {
+        '/api': {
+          target: env.VITE_DEV_API_BASE,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    },
+    define: {
+      ...useGitInfo(),
     }
-  },
-  server: {
-    port: 5174
-  },
-  define: {
-    ...useGitInfo(),
-  }
+  };
 });
