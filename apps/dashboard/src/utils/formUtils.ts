3343c593730736f64a49babbed2da5679b817555
Samuel <samuelincognito@gmail.com>
2024-07-30 23:09:47 +0200

    feat: stronger typing

--- apps/dashboard/src/utils/formUtils.ts
+++ apps/dashboard/src/utils/formUtils.ts
@@ -3,36 +3,37 @@ import type { Ref } from "vue";
 import type { BaseFieldProps, FormContext, GenericObject, TypedSchema } from "vee-validate";
 import { toTypedSchema } from "@vee-validate/yup";
 import { useForm } from "vee-validate";
+import type { AnyObject } from "yup";
 
-export interface Form<T> {
-  context: FormContext
-  schema:  TypedSchema
-  model: { [K in keyof T]: { value: Ref, attr: Ref<BaseFieldProps & GenericObject> } }
-  submit: () => Promise<any>
+export interface Form<T extends yup.AnyObject> {
+  context: FormContext<T, T>;
+  schema: TypedSchema<T>;
+  model: { [K in keyof T]: { value: Ref<T[K]>, attr: Ref<BaseFieldProps & GenericObject> } };
+  submit: () => Promise<any>;
 }
 
-const specToFields = <T extends Record<string, yup.AnySchema>>(spec: T, ctx: FormContext):
-  { [K in keyof T]: { value: Ref, attr: Ref<BaseFieldProps & GenericObject> } } => {
-  const fields = {} as { [K in keyof T]: { value: Ref, attr: Ref<BaseFieldProps & GenericObject> } };
-  for (const key in spec) {
-    if (Object.prototype.hasOwnProperty.call(spec, key)) {
+
+const schemaToFields = <T extends AnyObject>(yupSchema: yup.ObjectSchema<T, yup.AnyObject, T, "">, ctx: FormContext):
+  { [K in keyof T['fields']]: { value: Ref, attr: Ref<BaseFieldProps & GenericObject> } } => {
+  const fields = {} as { [K in keyof T['fields']]: { value: Ref, attr: Ref<BaseFieldProps & GenericObject> } };
+  for (const key in yupSchema['fields']) {
+    if (Object.prototype.hasOwnProperty.call(yupSchema['fields'], key)) {
       const f = ctx.defineField(key as keyof typeof ctx.defineField);
-      fields[key] = { value: f[0], attr: f[1] };
+      fields[key as keyof T['fields']] = { value: f[0], attr: f[1] };
     }
   }
   return fields;
 };
 
-export function specToForm<T extends Record<string, yup.AnySchema>>(spec: T): Form<T> {
-  const schema = toTypedSchema(
-    yup.object(spec)
-  );
+export function schemaToForm<T extends AnyObject>(yupSchema: yup.ObjectSchema<T>): Form<T> {
+  const schema = toTypedSchema(yupSchema) as TypedSchema<T>; // Convert to typed schema
 
-  const context = useForm({
+  const context: FormContext<T,T> = useForm<T>({
     validationSchema: schema,
   });
 
-  const model = specToFields(spec, context);
+
+  const model = schemaToFields<T>(yupSchema, context);
   return {
     context,
     schema,
