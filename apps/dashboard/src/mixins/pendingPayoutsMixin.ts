28c19e1175c2fffe34d0bc7a2927b36d60a658cc
Samuel <samuelincognito@gmail.com>
2024-08-11 23:08:41 +0200

    fix: build errors

--- apps/dashboard/src/mixins/pendingPayoutsMixin.ts
+++ apps/dashboard/src/mixins/pendingPayoutsMixin.ts
@@ -1,18 +1,25 @@
 // src/mixins/pendingPayoutsMixin.ts
-import { computed, type ComputedRef } from 'vue';
+import { computed, type ComputedRef, watch } from 'vue';
 import { usePayoutStore } from '@/stores/payout.store';
 import { useUserStore } from '@sudosos/sudosos-frontend-common';
 import { UserRole } from '@/utils/rbacUtils';
 
 export function usePendingPayouts() {
   const userStore = useUserStore();
-  let pendingPayouts: ComputedRef<number> | undefined;
+  const payoutStore = usePayoutStore();
+  const pendingPayouts = computed(() => payoutStore.pending);
 
-  if (userStore.current.rolesWithPermissions.findIndex(r => r.name == UserRole.BAC_PM) != -1) {
-    const payoutStore = usePayoutStore();
-    payoutStore.fetchPending();
-    pendingPayouts = computed(() => payoutStore.pending);
+  function updatePendingPayouts() {
+   if (userStore.current.rolesWithPermissions.findIndex(r => r.name == UserRole.BAC_PM) != -1) {
+      const payoutStore = usePayoutStore();
+      payoutStore.fetchPending();
+    }
   }
 
+  watch(() => userStore.current.rolesWithPermissions, () => {
+    updatePendingPayouts();
+  });
+
+  updatePendingPayouts();
   return { pendingPayouts };
 }
