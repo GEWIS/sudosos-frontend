bb050a7f50b7108d5f8ec90944d5f2f0a2546724
CodeNamedRobin <robinvandijke2002@gmail.com>
2024-09-09 11:31:52 +0200

    refactor(dashboard/profile): remove unused code

--- apps/dashboard/src/modules/user/views/UserProfileView.vue
+++ apps/dashboard/src/modules/user/views/UserProfileView.vue
@@ -12,23 +12,19 @@
 
 <script setup lang="ts">
 
-import { useAuthStore, useUserStore } from "@sudosos/sudosos-frontend-common";
+import { useUserStore } from "@sudosos/sudosos-frontend-common";
 import { storeToRefs } from "pinia";
 import type { GewisUserResponse, UserResponse } from "@sudosos/sudosos-client";
 import { onMounted, type Ref, ref } from "vue";
 import apiService from "@/services/ApiService";
 import router from "@/router";
-import { useToast } from "primevue/usetoast";
 import { useI18n } from "vue-i18n";
-import { handleError } from "@/utils/errorUtils";
 import UserSettingsComponent from "@/modules/user/components/UserSettingsComponent.vue";
 import UserInfo from "@/modules/user/components/UserInfo.vue";
 
 const userStore = useUserStore();
-const authStore = useAuthStore();
 const dataAnalysis: Ref<boolean> = ref(false);
 const { current } = storeToRefs(userStore);
-const toast = useToast();
 const { t } = useI18n();
 const gewisUser: Ref<GewisUserResponse | undefined> = ref(undefined);
 
