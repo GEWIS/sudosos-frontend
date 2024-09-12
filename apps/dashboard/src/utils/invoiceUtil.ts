70745a0a2072cdf1960566907c25e6a66976a41e
Samuel <samuelincognito@gmail.com>
2024-09-12 11:59:02 +0200

    fix: build errors

--- apps/dashboard/src/utils/invoiceUtil.ts
+++ apps/dashboard/src/utils/invoiceUtil.ts
@@ -2,5 +2,6 @@ import type { InvoiceResponse } from "@sudosos/sudosos-client";
 
 export function isDirty(invoice: InvoiceResponse): boolean {
   const entryTotal = invoice.invoiceEntries.reduce((sum, entry) => sum + entry.priceInclVat.amount * entry.amount, 0);
+  if (!invoice.transfer) return false;
   return entryTotal !== invoice.transfer.amountInclVat.amount;
 }
