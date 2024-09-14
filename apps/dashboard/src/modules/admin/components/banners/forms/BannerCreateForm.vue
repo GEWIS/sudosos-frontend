0679301cf75dc5dabf7750d76e0706f542e723ee
CodeNamedRobin <robinvandijke2002@gmail.com>
2024-09-14 18:39:29 +0200

    refactor(dashboard/banners): started refactor of banner create and edit forms into using new forms

---
+++ apps/dashboard/src/modules/admin/components/banners/forms/BannerCreateForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="flex flex-column gap-2">
+    <InputSpan :label="t('common.name')"
+               :value="form.model.name.value.value"
+               :attributes="form.model.name.attr.value"
+               @update:value="form.context.setFieldValue('name', $event)"
+               :errors="form.context.errors.value.name"
+               :disabled="!edit"
+               id="name" :placeholder="t('common.placeholders.banner')" type="text" />
+    <InputSpan :label="t('modules.admin.forms.banner.duration')"
+               :value="form.model.duration.value.value"
+               :attributes="form.model.duration.attr.value"
+               @update:value="form.context.setFieldValue('duration', $event)"
+               :errors="form.context.errors.value.duration"
+               :disabled="!edit"
+               id="duration" type="number" />
+    <InputSpan :label="t('modules.admin.forms.banner.timespan')"
+               :value="form.model.daterange.value.value"
+               :attributes="form.model.daterange.attr.value"
+               @update:value="form.context.setFieldValue('daterange', $event)"
+               :errors="form.context.errors.value.daterange"
+               :disabled="!edit"
+               id="daterange" type="daterange" />
+    <InputSpan :label="t('common.active')"
+               :value="form.model.active.value.value"
+               :attributes="form.model.active.attr.value"
+               @update:value="form.context.setFieldValue('active', $event)"
+               :errors="form.context.errors.value.active"
+               :disabled="!edit"
+               id="active" type="boolean" />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { useI18n } from "vue-i18n";
+import type { createBannerSchema } from "@/utils/validation-schema";
+import { type Form, setSubmit } from "@/utils/formUtils";
+import type { PropType } from "vue";
+import apiService from "@/services/ApiService";
+import type { BannerRequest } from "@sudosos/sudosos-client";
+import { handleError } from "@/utils/errorUtils";
+import InputSpan from "@/components/InputSpan.vue";
+import { useToast } from "primevue/usetoast";
+import * as yup from "yup";
+
+const { t } = useI18n();
+const toast = useToast();
+
+const emit = defineEmits(['update:edit']);
+
+const props = defineProps({
+  form: {
+    type: Object as PropType<Form<yup.InferType<typeof createBannerSchema>>>,
+    required: true,
+  },
+  edit: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+});
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ apps/dashboard/src/modules/admin/components/banners/forms/BannerImageForm.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="flex flex-column align-items-center gap-3">
+    <div class="picture-container" @click="triggerUpload">
+      <img v-if="imageSrc" class="product-image" :src="imageSrc" />
+      <div v-else class="no-banner">
+        <p class="no-banner-text">{{ t('modules.admin.banners.noBannerFound') }}</p>
+        <input
+            ref="fileInput"
+            type="file"
+            class="hidden-file-input"
+            accept="image/*"
+            @change="handleFileUpload"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n();
+
+defineProps<{
+  imageSrc?: string,
+}>();
+
+const fileInput = ref<HTMLInputElement | null>(null);
+
+const emit = defineEmits<{
+  upload: [image: File]
+}>();
+
+const triggerUpload = () => {
+  if (fileInput.value) {
+    fileInput.value.click();
+  }
+};
+
+const handleFileUpload = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files[0]) {
+    emit('upload', target.files[0]);
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.picture-container {
+  width: 800px; /* Set a fixed width */
+  position: relative;
+  padding-bottom: calc((7 / 52) * 100%); /* Maintain aspect ratio of 52:7 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  background-color: lightgray;
+}
+
+.no-banner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: lightgray;
+  color: black;
+  cursor: pointer;
+}
+
+.no-banner-text {
+  color: black;
+  font-size: 1.2em;
+  text-align: center;
+}
+
+.hidden-file-input {
+  display: none;
+}
+
+.product-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
