917dfe467c6a4e2f8226a38077c230fec6941243
Samuel <samuel.oosterholt20001004@gmail.com>
2024-12-24 19:03:52 +0100

    fix(dashboard): set background color of maintenance to red

--- apps/dashboard/src/views/MaintenanceView.vue
+++ apps/dashboard/src/views/MaintenanceView.vue
@@ -1,22 +1,20 @@
 <template>
-    <div class="flex flex-column h-screen bg-primary-700">
-      <main class="flex flex-column align-items-center justify-content-center flex-grow text-center
-      h-full mx-auto my-7 max-w-30rem">
-        <img class="max-h-9rem block mx-auto" src="../assets/img/bier_grayscale.png" alt="logo"/>
-        <div class="text-white text-2xl text-center">{{ t('modules.auth.maintenance.maintenance') }}</div>
-        <div class="text-white text-1xl text-center">{{ t('modules.auth.maintenance.tryAgain') }}</div>
-      </main>
-    </div>
+  <div class="flex flex-column h-screen bg-primary">
+    <main class="flex flex-column align-items-center justify-content-center flex-grow text-center
+    h-full mx-auto my-7 max-w-30rem">
+      <img class="max-h-9rem block mx-auto mb-5" src="../assets/img/bier_grayscale.png" alt="logo"/>
+      <div class="text-white font-bold text-3xl text-center">{{ t('modules.auth.maintenance.maintenance') }}</div>
+      <div class="text-white text-2xl text-center">{{ t('modules.auth.maintenance.tryAgain') }}</div>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
-
 import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
-
 </script>
 
-<style>
+<style scoped lang="scss">
 
 </style>
