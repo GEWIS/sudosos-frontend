26c60172ae20079e687da21ebab517cd19bfc21c
Samuel <samuelincognito@gmail.com>
2025-04-01 20:22:55 +0200

    feat(dashboard/financial): improve invoice table by grouping per year instead of state

--- apps/dashboard/src/modules/financial/components/invoice/InvoiceTable.vue
+++ apps/dashboard/src/modules/financial/components/invoice/InvoiceTable.vue
@@ -4,65 +4,102 @@
         :rows="rows"
         :value="invoices"
         :rows-per-page-options="[5, 10, 25, 50, 100]"
-        :paginator="paginator"
+        :paginator="true"
         lazy
-        @page="onPage($event)"
+        @page="onPage"
         :total-records="totalRecords"
+        :filters="filters"
         data-key="id"
         class="w-full"
         tableStyle="min-width: 50rem"
+        filterDisplay="menu"
     >
       <Column field="date" :header="t('common.date')">
         <template #body="slotProps">
-          <Skeleton v-if="isLoading" class="w-6 my-1 h-1rem surface-300" />
-          <span v-else>
-                        {{ formatDateFromString(slotProps.data.createdAt) }}
-                    </span>
+          <div class="cell-content">
+            <Skeleton v-if="isLoading" class="skeleton-fixed w-6 surface-300" />
+            <span v-else>
+              {{ formatDateFromString(slotProps.data.createdAt) }}
+            </span>
+          </div>
         </template>
       </Column>
-      <Column field="currentState.state" :header="t('common.status')">
+
+      <Column
+          field="currentState.state"
+          :header="t('common.status')"
+          filter
+          filterMatchMode="equals"
+          :showFilterMatchModes="false"
+          :showApplyButton="false"
+          :showClearButton="false"
+      >
+        <template #filter="{ filterModel }">
+          <Dropdown
+              v-model="filterModel.value"
+              @change="stateFilterChange"
+              :options="states"
+              optionLabel="name"
+              optionValue="value"
+              :placeholder="t('common.placeholders.selectType')"
+          />
+        </template>
         <template #body="slotProps">
-          <Skeleton v-if="isLoading" class="w-6 my-1 h-1rem surface-300" />
-          <span v-else>
-                        {{ slotProps.data.currentState.state }}
-                    </span>
+          <div class="cell-content">
+            <Skeleton v-if="isLoading" class="skeleton-fixed w-6 surface-300" />
+            <span v-else>
+              {{ slotProps.data.currentState.state }}
+            </span>
+          </div>
         </template>
       </Column>
-      <Column field="to.firstName" :header="t('common.for')">
+
+      <Column field="to.firstName" :header="t('common.for')" style="max-width: 10rem;">
         <template #body="slotProps">
-          <Skeleton v-if="isLoading" class="w-6 my-1 h-1rem surface-300" />
-          <span v-else>
-                        {{ slotProps.data.to.firstName }}
-                    </span>
+          <div class="cell-content">
+            <Skeleton v-if="isLoading" class="skeleton-fixed w-6 surface-300" />
+            <span v-else class="truncate">
+              {{ slotProps.data.to.firstName }}
+            </span>
+          </div>
         </template>
       </Column>
-      <Column field="description" :header="t('common.description')" style="max-width: 15rem">
+
+      <Column field="description" :header="t('common.description')" style="max-width: 15rem;">
         <template #body="slotProps">
-          <Skeleton v-if="isLoading" class="w-6 my-1 h-1rem surface-300" />
-          <span v-else>
-                        {{ slotProps.data.description }}
-                    </span>
+          <div class="cell-content">
+            <Skeleton v-if="isLoading" class="skeleton-fixed w-6 surface-300" />
+            <span v-else class="truncate">
+              {{ slotProps.data.description }}
+            </span>
+          </div>
         </template>
       </Column>
+
       <Column field="transfer.amount" :header="t('common.amount')">
         <template #body="slotProps">
-          <Skeleton v-if="isLoading" class="w-3 my-1 h-1rem surface-300" />
-          <span v-else>
-                        {{ formatPrice(slotProps.data.transfer?.amount) }}
-                    </span>
+          <div class="cell-content">
+            <Skeleton v-if="isLoading" class="skeleton-fixed w-3 surface-300" />
+            <span v-else>
+              {{ formatPrice(slotProps.data.transfer?.amount) }}
+            </span>
+          </div>
         </template>
       </Column>
+
       <Column :header="t('common.actions')" style="width: 10%">
         <template #body="slotProps">
-          <Skeleton v-if="isLoading" class="w-3 my-1 h-1rem surface-300" />
-          <span v-else>
-                        <Button
-                            type="button"
-                            icon="pi pi-eye"
-                            class="p-button-rounded p-button-text p-button-plain"
-                            @click="() => viewInvoice(slotProps.data.id)"
-                        />
-                    </span>
+          <div class="cell-content">
+            <Skeleton v-if="isLoading" class="skeleton-fixed w-3 surface-300" />
+            <span v-else>
+              <Button
+                  type="button"
+                  icon="pi pi-eye"
+                  class="p-button-rounded p-button-text p-button-plain"
+                  @click="() => viewInvoice(slotProps.data.id)"
+              />
+            </span>
+          </div>
         </template>
       </Column>
     </DataTable>
@@ -70,58 +107,87 @@
 </template>
 
 <script setup lang="ts">
-import DataTable, { type DataTablePageEvent } from 'primevue/datatable';
+import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import { ref, onMounted, type PropType } from "vue";
-import { useInvoiceStore } from "@/stores/invoice.store";
+import Skeleton from 'primevue/skeleton';
+import Button from 'primevue/button';
+import Dropdown from 'primevue/dropdown';
+import { useI18n } from 'vue-i18n';
 import { formatPrice, formatDateFromString } from "@/utils/formatterUtils";
-import type { InvoiceResponse } from "@sudosos/sudosos-client";
-import { InvoiceStatusResponseStateEnum } from "@sudosos/sudosos-client/src/api";
 import router from "@/router";
-import { useI18n } from "vue-i18n";
+import { InvoiceStatusResponseStateEnum } from "@sudosos/sudosos-client/src/api";
+import { type Ref, ref } from "vue";
+
+const props = defineProps({
+  invoices: {
+    type: Array,
+    required: true
+  },
+  totalRecords: {
+    type: Number,
+    required: true
+  },
+  isLoading: {
+    type: Boolean,
+    required: true
+  },
+  rows: {
+    type: Number,
+    required: true
+  }
+});
+
+const emit = defineEmits(['page', 'stateFilterChange']);
 
 const { t } = useI18n();
 
-const invoiceStore = useInvoiceStore();
-const totalRecords = ref<number>(0);
-const isLoading = ref<boolean>(true);
+function onPage(event: any) {
+  emit('page', event);
+}
 
-const invoices = ref<InvoiceResponse[]>([]);
+function stateFilterChange(e: any) {
+  console.log(e);
+  emit('stateFilterChange', e);
+}
 
-const rows = ref<number>(10);
-const paginator = ref<boolean>(true);
+const states: Ref<Array<{name: string, value: string | null}>> = ref([
+  { name: InvoiceStatusResponseStateEnum.Created, value: InvoiceStatusResponseStateEnum.Created },
+  { name: InvoiceStatusResponseStateEnum.Sent, value: InvoiceStatusResponseStateEnum.Sent },
+  { name: InvoiceStatusResponseStateEnum.Paid, value: InvoiceStatusResponseStateEnum.Paid },
+  { name: InvoiceStatusResponseStateEnum.Deleted, value: InvoiceStatusResponseStateEnum.Deleted },
+  { name: 'ALL', value: null }
+]);
 
-const viewInvoice = async (id: number) => {
+async function viewInvoice(id: number) {
   let route = router.resolve({ name: 'invoiceInfo', params: { id } });
   window.open(route.href, '_blank');
-};
-
-const props = defineProps({
-  state: {
-    type: String as PropType<InvoiceStatusResponseStateEnum>,
-    required: true,
-  }
-});
-
-onMounted(async () => {
-  await loadInvoices();
-});
-
-async function loadInvoices(skip = 0) {
-  isLoading.value = true;
-  const response = await invoiceStore.fetchInvoices(rows.value, skip, props.state);
-  if (response) {
-    invoices.value = response.records as InvoiceResponse[];
-    totalRecords.value = response._pagination.count || 0;
-  }
-  isLoading.value = false;
 }
 
-async function onPage(event: DataTablePageEvent) {
-  await loadInvoices(event.first);
-}
+const filters = ref({
+  'currentState.state': { value: null, matchMode: 'equals' }
+});
 </script>
 
 <style scoped lang="scss">
-/* Add your custom styles here */
+.truncate {
+  display: block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.cell-content {
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+  box-sizing: border-box;
+}
+
+
+.skeleton-fixed {
+  height: 1rem;
+  margin: 0;
+  display: block;
+}
 </style>
