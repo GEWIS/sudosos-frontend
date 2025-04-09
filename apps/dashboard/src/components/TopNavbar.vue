345a7205187816535da64aa586f0569efd0a07d4
Samuel <samuel.oosterholt20001004@gmail.com>
2025-04-09 12:06:05 +0200

    fix: invoice account overview loading and notification
    Also includes reactivity improvements.

--- apps/dashboard/src/components/TopNavbar.vue
+++ apps/dashboard/src/components/TopNavbar.vue
@@ -102,7 +102,6 @@ const handleLogout = () => {
 };
 
 const { pendingPayouts } = usePendingPayouts();
-
 const { openInvoiceAccounts } = useOpenInvoiceAccounts();
 const getFinancialNotifications = () => isAllowed('update', ['all'], 'SellerPayout', ['any']) && pendingPayouts?.value + openInvoiceAccounts?.value;
 
