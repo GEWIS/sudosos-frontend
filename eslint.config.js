31cc78b9c71937385ff73f33c582c46528db63fa
Victor Klomp <victor@gewis.nl>
2025-04-11 18:23:59 +0200

    refactor(eslint): use GEWIS/js-config files

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "eslint/config";
+import { eslintConfig as common } from '@gewis/eslint-config-typescript';
+import { eslintConfig as vue } from '@gewis/eslint-config-vue';
+import { eslintConfig as prettier } from '@gewis/prettier-config';
+
+export default defineConfig([
+    {
+        extends: [common, vue, prettier],
+        rules: {
+            "no-unused-vars": "warn",
+        },
+    },
+]);
