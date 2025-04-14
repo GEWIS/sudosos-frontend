6eccce53e6241784e4c01307cc8ff6f84829fd49
Noël van de Vrie <46963141+noelvandevrie@users.noreply.github.com>
2025-04-14 13:24:22 +0200

    feat(pos): green borrel splash for BAC (#540)

--- apps/point-of-sale/src/App.vue
+++ apps/point-of-sale/src/App.vue
@@ -1,26 +1,12 @@
 <script setup lang="ts">
 import Toast from 'primevue/toast';
+import SplashComponent from '@/components/SplashComponent.vue';
 </script>
 
 <template>
   <Toast />
   <RouterView />
-  <img alt="Overlay Image" class="image-underlay" src="@/assets/splash.svg" />
+  <SplashComponent />
 </template>
 
-<style lang="scss">
-.image-underlay {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  opacity: 1;
-  pointer-events: none;
-  z-index: -10;
-  overflow: hidden;
-
-  position: fixed;
-  top: -25%;
-  right: 0;
-  height: 175%;
-}
-</style>
+<style lang="scss"></style>
