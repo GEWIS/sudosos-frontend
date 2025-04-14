2cded3e3056bceb2746bd68d17ee3f3a0b269ee3
Noël van de Vrie <nvdvrie@gmail.com>
2025-04-14 18:43:37 +0200

    fix: lint/prettier

--- apps/point-of-sale/src/components/GitInfo.vue
+++ apps/point-of-sale/src/components/GitInfo.vue
@@ -13,7 +13,6 @@ const commit: string | undefined = import.meta.env.VITE_GIT_COMMIT_SHA;
 const localBuild = computed(() => {
   if (typeof branch !== 'string' || branch.length === 0) return true;
   return typeof commit !== 'string' || commit.length === 0;
-
 });
 </script>
 
