64ba31440eb90c9fe2cf4f0270e83d7b85d0f323
Samuel <samuel.oosterholt20001004@gmail.com>
2025-04-19 13:41:49 +0200

    fix: remove relative base from dashboard

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -7,7 +7,6 @@ const PROXY_URL = 'https://sudosos.test.gewis.nl';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    base: './',
     plugins: [vue()],
     resolve: {
       alias: {
