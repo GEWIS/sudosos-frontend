2c3c4f4b59acbe10cf2d05a3c0422e4fc8ba8eb2
RubenLWF <ruben.lagerwerf@gmail.com>
2025-06-16 21:34:52 +0200

    fix: force 24-hour time format

--- apps/point-of-sale/src/utils/FormatUtils.ts
+++ apps/point-of-sale/src/utils/FormatUtils.ts
@@ -1,7 +1,7 @@
 import { DineroObjectResponse } from '@sudosos/sudosos-client/dist/api';
 
 const locale: Intl.LocalesArgument = Intl.DateTimeFormat().resolvedOptions().locale;
-const timeFormatOptions: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit' };
+const timeFormatOptions: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit', hour12: false };
 
 export function formatDateFromString(date: string | undefined) {
   if (!date) return '';
