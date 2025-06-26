39f3a882ffe1ec4dbf3239a1d61d5af2c2bc10b8
Samuel <samuelincognito@gmail.com>
2025-06-26 17:35:43 +0200

    refactor: move auth local to new card

--- apps/dashboard/src/modules/auth/views/AuthLocalView.vue
+++ apps/dashboard/src/modules/auth/views/AuthLocalView.vue
@@ -1,27 +1,16 @@
 <template>
-  <div>
-    <img alt="logo" class="block max-h-[14rem] mx-auto my-0" src="../../../assets/img/bier.png" />
-    <div class="my-6 mx-auto text-5xl text-900 w-full">{{ t('modules.auth.login.sudosos') }}</div>
-    <Card>
-      <template #content>
-        <AuthLocalForm :form="form" />
-        <Button
-          id="login-button"
-          class="items-center flex justify-center mx-auto w-full"
-          type="submit"
-          @click="formSubmit"
-        >
-          {{ t('modules.auth.login.login') }}
-        </Button>
-        <Button class="items-center flex justify-center mx-auto my-3 w-full" :outlined="true" @click="toHomeView">
-          {{ t('common.back') }}
-        </Button>
-        <div class="cursor-pointer text-900 underline" @click="resetPassword">
-          {{ t('modules.auth.reset.passwordReset') }}
-        </div>
-      </template>
-    </Card>
-  </div>
+  <AuthLocalCard :header="t('modules.auth.login.sudosos')">
+    <AuthLocalForm :form="form" />
+    <Button id="login-button" class="items-center flex justify-center mx-auto w-full" type="submit" @click="formSubmit">
+      {{ t('modules.auth.login.login') }}
+    </Button>
+    <Button class="items-center flex justify-center mx-auto my-3 w-full" :outlined="true" @click="toHomeView">
+      {{ t('common.back') }}
+    </Button>
+    <div class="cursor-pointer text-900 underline" @click="resetPassword">
+      {{ t('modules.auth.reset.passwordReset') }}
+    </div>
+  </AuthLocalCard>
 </template>
 
 <script setup lang="ts">
@@ -34,6 +23,7 @@ import apiService from '@/services/ApiService';
 import { schemaToForm } from '@/utils/formUtils';
 import { localAuthForm } from '@/utils/validation-schema';
 import AuthLocalForm from '@/modules/auth/components/forms/AuthLocalForm.vue';
+import AuthLocalCard from '@/modules/auth/components/AuthLocalCard.vue';
 
 const { t } = useI18n();
 const authStore = useAuthStore();
