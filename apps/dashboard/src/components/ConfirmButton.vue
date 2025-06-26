51fc4dab972279236d0bdb1125a5542441646a99
Samuel <samuelincognito@gmail.com>
2025-06-26 02:03:25 +0200

    fix: improve confirm button clicked once styling

--- apps/dashboard/src/components/ConfirmButton.vue
+++ apps/dashboard/src/components/ConfirmButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <Button :disabled="disabled" :icon="icon" :label="labelText" :type="type" @click="handleClick" />
+  <Button :disabled="disabled" :icon="icon" :label="labelText" :severity="severity" :type="type" @click="handleClick" />
 </template>
 
 <script lang="ts" setup>
@@ -21,6 +21,8 @@ const clickedOnce = ref(false);
 const labelText = computed(() =>
   clickedOnce.value ? (props.confirmLabel ?? 'Confirm') : (props.initialLabel ?? 'Save'),
 );
+
+const severity = computed(() => (clickedOnce.value ? 'danger' : 'primary'));
 
 function handleClick() {
   if (clickedOnce.value) {
