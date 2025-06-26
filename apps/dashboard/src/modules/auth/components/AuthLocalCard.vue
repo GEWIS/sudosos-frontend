2a2ea4c5413e84b1c34cfcaa11eb6067f3e9615f
Samuel <samuelincognito@gmail.com>
2025-06-26 17:35:20 +0200

    feat: add auth local card for layout

---
+++ apps/dashboard/src/modules/auth/components/AuthLocalCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img alt="logo" class="block max-h-[14rem] mx-auto my-0" src="@/assets/img/bier.png" />
+    <div v-if="header" class="my-6 mx-auto text-5xl text-900 w-full">
+      {{ header }}
+    </div>
+    <Card>
+      <template #content>
+        <slot />
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  header?: string;
+}>();
+</script>
