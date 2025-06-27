9035770a5d2179c3a45c3e8405aef70cb7d71dff
Samuel <samuel@gewis.nl>
2025-06-27 11:46:43 +0200

    fix: tos styling using prose (#590)

--- apps/dashboard/src/modules/auth/views/AuthTermsOfServiceView.vue
+++ apps/dashboard/src/modules/auth/views/AuthTermsOfServiceView.vue
@@ -1,27 +1,30 @@
 <template>
-  <PageContainer>
-    <h1 class="text-4xl mb-4">{{ t('modules.auth.tos.acceptFirst') }}</h1>
-    <p>{{ t('modules.auth.tos.description') }}</p>
-    <hr />
-    <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
-    <p>SudoSOS Terms of Service - version 1.0 (14/08/2022)</p>
-    <!-- eslint-disable-next-line vue/no-v-html -->
-    <div v-html="tos" />
+  <PageContainer class="m-6 w-full mx-auto">
+    <article class="prose prose-inherit mx-auto max-w-4xl">
+      <h1 class="text-4xl mb-4">{{ t('modules.auth.tos.acceptFirst') }}</h1>
+      <p>{{ t('modules.auth.tos.description') }}</p>
+      <Divider />
+      <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
+      <p>SudoSOS Terms of Service - version 1.0 (14/08/2022)</p>
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <div v-html="tos" />
 
-    <Checkbox v-model="acceptsExtensiveDataProcessing" :binary="true" class="checkbox" />
-    <label for="accept">{{ t('modules.auth.tos.agreeData') }}</label>
+      <div class="flex items-center my-4 gap-2">
+        <Checkbox v-model="acceptsExtensiveDataProcessing" :binary="true" class="checkbox" />
+        <label for="accept">{{ t('modules.auth.tos.agreeData') }}</label>
+      </div>
 
-    <div class="tos-button-box">
-      <Button severity="secondary" type="button" @click="handleLogout">
-        {{ t('common.signOut') }}
-      </Button>
-      <Button type="button" @click="acceptTermsOfService">
-        {{ t('modules.auth.tos.agreeToS') }}
-      </Button>
-    </div>
+      <div class="flex justify-end gap-4 mt-4">
+        <Button severity="secondary" type="button" @click="handleLogout">
+          {{ t('common.signOut') }}
+        </Button>
+        <Button type="button" @click="acceptTermsOfService">
+          {{ t('modules.auth.tos.agreeToS') }}
+        </Button>
+      </div>
+    </article>
   </PageContainer>
 </template>
-
 <script setup lang="ts">
 import { marked } from 'marked';
 import { useAuthStore, useUserStore } from '@sudosos/sudosos-frontend-common';
@@ -68,23 +71,15 @@ const handleLogout = () => {
 </script>
 
 <style scoped lang="scss">
-.checkbox {
-  margin-right: 1rem;
+.prose {
+  color: var(--text-color-contrast) !important;
 }
 
-p {
-  color: black;
+.prose :is(h1, h2, h3, h4, h5, h6) {
+  color: var(--text-color-contrast) !important;
 }
 
-.page-container {
-  margin: 1.5rem !important;
-}
-
-.tos-button-box {
-  margin-top: 1rem;
-}
-
-.tos-button-box > * {
-  margin-right: 1rem;
+.prose :is(b, strong) {
+  color: var(--text-color-contrast) !important;
 }
 </style>
