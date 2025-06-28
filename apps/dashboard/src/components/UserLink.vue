c26e8898eab35762ecaf750dc2dd5487ec99e258
Samuel <samuel@gewis.nl>
2025-06-29 00:28:38 +0200

    feat(dashboard): show user link based on permissions (#600)

--- apps/dashboard/src/components/UserLink.vue
+++ apps/dashboard/src/components/UserLink.vue
@@ -1,24 +1,36 @@
 <template>
-  <a class="cursor-pointer hover:opacity-80 text-primary" @click="goToUser">
-    {{ user.firstName }} {{ user.lastName }}
+  <a v-if="allowed" class="cursor-pointer hover:opacity-80 text-primary" @click="goToUser">
+    {{ name }}
   </a>
+  <span v-else>{{ name }}</span>
 </template>
 
 <script setup lang="ts">
 import type { BaseUserResponse } from '@sudosos/sudosos-client';
-import type { PropType } from 'vue';
+import { computed } from 'vue';
 import router from '@/router';
+import { getRelation, isAllowed } from '@/utils/permissionUtils';
 
-const props = defineProps({
-  user: {
-    type: Object as PropType<BaseUserResponse>,
-    required: true,
-  },
-  newTab: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
+/**
+ * Props for UserLink component.
+ * @property user - The user to link to.
+ * @property [newTab=false] - Open in new tab if true.
+ */
+interface UserLinkProps {
+  user: BaseUserResponse;
+  newTab?: boolean;
+}
+
+/**
+ * Renders a link to a user's profile page.
+ */
+const props = defineProps<UserLinkProps>();
+
+// If the user cannot get the balance, there is not much sensible to see at the profile page anyway.
+const allowed = isAllowed('get', [getRelation(props.user.id)], 'Balance', ['any']);
+
+const name = computed(() => {
+  return `${props.user.firstName} ${props.user.lastName}`.trim();
 });
 
 const goToUser = () => {
