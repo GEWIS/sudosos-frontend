b19dc916e60be1c1b2b81711a5a2e28c9c67e345
Samuel <samuel@gewis.nl>
2025-06-30 13:07:26 +0200

    fix(dashboard): userlink not showing `you` (#605)

--- apps/dashboard/src/components/UserLink.vue
+++ apps/dashboard/src/components/UserLink.vue
@@ -8,8 +8,13 @@
 <script setup lang="ts">
 import type { BaseUserResponse } from '@sudosos/sudosos-client';
 import { computed } from 'vue';
+import { useAuthStore } from '@sudosos/sudosos-frontend-common';
+import { useI18n } from 'vue-i18n';
 import router from '@/router';
 import { getRelation, isAllowed } from '@/utils/permissionUtils';
+
+const { t } = useI18n();
+const authStore = useAuthStore();
 
 /**
  * Props for UserLink component.
@@ -29,7 +34,10 @@ const props = defineProps<UserLinkProps>();
 // If the user cannot get the balance, there is not much sensible to see at the profile page anyway.
 const allowed = isAllowed('get', [getRelation(props.user.id)], 'Balance', ['any']);
 
+const u = authStore.getUser;
+
 const name = computed(() => {
+  if (u?.id === props.user.id) return t('components.mutations.you');
   return `${props.user.firstName} ${props.user.lastName}`.trim();
 });
 
