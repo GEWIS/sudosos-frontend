eef1f84944aff1747164ed205d7abb8896606f99
Victor Klomp <victor@gewis.nl>
2025-11-15 21:27:06 +0100

    fix: remove settings in loginview, add posinfo text loginview

---
+++ apps/point-of-sale/src/components/PosInfo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="font-bold opacity-30 text-black">
+    <b>{{ posStore.getPos?.name || 'Unknown POS' }}</b>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { usePointOfSaleStore } from '@/stores/pos.store';
+
+const posStore = usePointOfSaleStore();
+</script>
