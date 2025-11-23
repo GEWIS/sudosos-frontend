0874443b6c242dee56d3ac929633df12f6d0f9e6
Ruben Lagerwerf <98549214+RubenLWF@users.noreply.github.com>
2025-11-23 13:33:59 +0100

    feat: Add admin login for maintenance mode (#680)
    * feat: Add admin login for maintenance mode
    
    Allows admins to log in using a local account when maintenance mode is enabled.
    An admin is any user that has a role with the maintenance override permission.
    
    * feat: Add missing translations for maintenance admin login
    
    Adds the Dutch and Polish translations for the strings added for maintenance mode admin login.
    
    * style: Position maintenance mode overlay fixed
    
    Changes the positioning for the MaintenanceModeOverlay from absolute to fixed.
    
    Before, the footer would sometimes show below the overlay if the screen was narrow enough.
    Giving the overlay position fixed makes it always cover the full page.

--- apps/dashboard/src/components/MaintenanceModeOverlay.vue
+++ apps/dashboard/src/components/MaintenanceModeOverlay.vue
@@ -1,26 +1,47 @@
 <template>
   <div
     v-if="isMaintenance && !canOverride"
-    class="absolute top-0 left-0 w-full h-full flex text-white justify-center items-center z-10 bg-red-700"
+    class="fixed top-0 left-0 w-full h-full flex text-white justify-center items-center z-10 bg-red-700"
   >
     <main>
       <img alt="logo" class="block mx-auto mb-5 max-h-[25rem]" src="@/assets/img/bier_grayscale.png" />
       <div class="font-bold text-3xl text-center">{{ t('common.maintenanceMode.title') }}</div>
       <div class="text-2xl text-center">{{ t('common.maintenanceMode.subtitle') }}</div>
+      <div class="text-center mt-5">
+        <a v-if="!showLogin" class="block text-center underline cursor-pointer" @click="toggleShowLogin">
+          {{ t('common.maintenanceMode.adminLogin') }}
+        </a>
+        <div v-else>
+          <Card>
+            <template #content>
+              <AuthLocalForm :form="form" />
+            </template>
+          </Card>
+          <a class="block text-center underline cursor-pointer mt-2" @click="toggleShowLogin">
+            {{ t('common.maintenanceMode.cancelLogin') }}
+          </a>
+        </div>
+      </div>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useAuthStore } from '@sudosos/sudosos-frontend-common';
-import { watch } from 'vue';
+import { ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
 import { useIsMaintenance } from '@/composables/isMaintenance';
+import AuthLocalForm from '@/modules/auth/components/forms/AuthLocalForm.vue';
+import { schemaToForm } from '@/utils/formUtils';
+import { localAuthForm } from '@/utils/validation-schema';
 
 const { t } = useI18n();
 const router = useRouter();
 const { isMaintenance, canOverride } = useIsMaintenance();
+const form = schemaToForm(localAuthForm);
+
+const showLogin = ref(false);
 
 watch(
   () => isMaintenance.value,
@@ -32,6 +53,13 @@ watch(
     }
   },
 );
+
+const toggleShowLogin = () => {
+  showLogin.value = !showLogin.value;
+  if (!showLogin.value) {
+    form.context.resetForm();
+  }
+};
 </script>
 
 <style scoped lang="scss"></style>
