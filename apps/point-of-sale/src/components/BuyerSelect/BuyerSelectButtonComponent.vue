7b08d97846f88e1698939373c8f7a21aa30cf187
Ruben Lagerwerf <98549214+RubenLWF@users.noreply.github.com>
2026-01-09 13:33:32 +0100

    fix: disable buyer select button after press (#719)
    The same transaction would be made multiple times if the buyer button
    was pressed multiple times before the first transaction was completed.
    
    This is fixed by disabling all buyer buttons after one is pressed.
    The text on the button that has been pressed is replaced with a spinner
    to indicate that the system is in fact doing something.

--- apps/point-of-sale/src/components/BuyerSelect/BuyerSelectButtonComponent.vue
+++ apps/point-of-sale/src/components/BuyerSelect/BuyerSelectButtonComponent.vue
@@ -2,10 +2,12 @@
   <Button
     v-if="!isGhost && associate"
     class="text-lg px-4 py-4 select-user"
+    :disabled="props.isDisabled"
     :outlined="associate.type !== 'owner'"
     @click="select()"
   >
-    <span>
+    <ProgressSpinner v-if="props.isLoading" stroke-width="4" style="width: 30px; height: 30px" />
+    <span v-else>
       {{ associate.firstName }}
       <span v-if="associate.nickname" class="italic"> "{{ associate.nickname }}"</span>
       {{ associate.lastName }}
@@ -16,6 +18,7 @@
 
 <script setup lang="ts">
 import Button from 'primevue/button';
+import ProgressSpinner from 'primevue/progressspinner';
 import { useCartStore } from '@/stores/cart.store';
 import { PointOfSaleAssociate } from '@/stores/pos.store';
 
@@ -25,19 +28,25 @@ const props = withDefaults(
   defineProps<{
     associate: PointOfSaleAssociate | null;
     isGhost?: boolean;
+    isDisabled?: boolean;
+    isLoading?: boolean;
   }>(),
   {
     associate: null,
     isGhost: false,
+    isDisabled: false,
+    isLoading: false,
   },
 );
 
 const emit = defineEmits<{
   cancelSelectCreator: [];
+  buttonClicked: [number];
 }>();
 
 const select = async () => {
   if (!props.associate) return;
+  emit('buttonClicked', props.associate.id);
   cartStore.setCreatedBy(props.associate);
   await cartStore.checkout();
   emit('cancelSelectCreator');
