a0ea61c3e93f83537c0c0fcf5670821a818f3173
Samuel <samuel@gewis.nl>
2026-01-19 23:33:05 +0100

    feat: User Settings (#732)

--- apps/dashboard/src/App.vue
+++ apps/dashboard/src/App.vue
@@ -2,10 +2,16 @@
 import { RouterView } from 'vue-router';
 import Toast from 'primevue/toast';
 import { useDarkMode } from '@/composables/darkMode';
+import { useBetaSync } from '@/composables/useBetaSync';
+import BetaSyncOverlay from '@/components/BetaSyncOverlay.vue';
+
 useDarkMode();
+
+const { showRedirectOverlay } = useBetaSync();
 </script>
 
 <template>
   <Toast />
+  <BetaSyncOverlay :visible="showRedirectOverlay" />
   <RouterView />
 </template>
