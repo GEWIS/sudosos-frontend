0e27cf6945a3e8e817ebda8d86a3db60c419cb4d
Robin van Dijke <robin.van.dijke@weeztix.com>
2026-02-20 12:13:12 +0100

    feat(dashboard): add financial overview table
    feat: temp

---
+++ apps/dashboard/src/modules/financial/views/overview/FinancialOverviewTable.vue
@@ -0,0 +1,126 @@
+<template>
+  <DataTable striped-rows :value="financialMutations">
+    <Column class="text-left" field="transferType" header="Type" header-class="font-sans" />
+
+    <Column field="credit" header="Credit" header-class="font-sans text-right" class="font-mono text-right">
+      <template #body="{ data }">{{ new Intl.NumberFormat('en-DE').format(data.credit || 0) }}</template>
+    </Column>
+
+    <Column field="debit" header="Debit" header-class="font-sans text-right" class="font-mono text-right">
+      <template #body="{ data }">{{ new Intl.NumberFormat('en-DE').format(data.debit || 0) }}</template>
+    </Column>
+
+    <Column header="Saldo" header-class="font-sans text-right" class="font-mono text-right">
+      <template #body="{ data }">
+        {{ new Intl.NumberFormat('en-DE').format((data.credit || 0) - (data.debit || 0)) }}
+      </template>
+    </Column>
+
+    <Column class="w-2" :header="t('common.actions')" header-class="font-sans">
+      <template #body>
+        <Button class="p-button-rounded p-button-text" icon="pi pi-window-maximize" />
+      </template>
+    </Column>
+
+    <ColumnGroup type="footer">
+      <Row>
+        <Column :colspan="3" footer="Total" />
+        <Column
+          class="font-mono text-right"
+          :footer="
+            new Intl.NumberFormat('en-DE').format(
+              financialMutations.reduce((acc, r) => acc + (r.credit || 0) - (r.debit || 0), 0),
+            )
+          "
+        />
+        <Column />
+      </Row>
+
+      <Row>
+        <Column :colspan="3" footer="Balance 01-07-2024" />
+        <Column :footer="new Intl.NumberFormat('en-DE').format(12345)" class="font-mono text-right" />
+        <Column />
+      </Row>
+
+      <Row>
+        <Column :colspan="3" footer="Balance 30-06-2025" />
+        <Column
+          class="font-mono text-right"
+          :footer="
+            new Intl.NumberFormat('en-DE').format(
+              12345 + financialMutations.reduce((acc, r) => acc + (r.credit || 0) - (r.debit || 0), 0),
+            )
+          "
+        />
+        <Column />
+      </Row>
+    </ColumnGroup>
+  </DataTable>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';
+import Row from 'primevue/row';
+import { useI18n } from 'vue-i18n';
+import type { UserResponse } from '@sudosos/sudosos-client';
+
+const props = defineProps<{
+  year: number;
+  sellers: UserResponse[];
+}>();
+
+interface FinancialOverviewTableRow {
+  transferType: string;
+  credit: number;
+  debit: number;
+  link: string;
+}
+
+const { t } = useI18n();
+
+const financialMutations = ref<FinancialOverviewTableRow[]>([
+  {
+    transferType: 'Topups',
+    credit: 100.0,
+    debit: 0.0,
+    link: '',
+  },
+  {
+    transferType: 'Sales',
+    credit: 1250.5,
+    debit: 0.0,
+    link: '/user/18214',
+  },
+  {
+    transferType: 'Fines',
+    credit: 0.0,
+    debit: 25.0,
+    link: '',
+  },
+  {
+    transferType: 'Seller Payouts',
+    credit: 0.0,
+    debit: 850.0,
+    link: '/financial/overview/seller-payouts',
+  },
+  {
+    transferType: 'Write-offs',
+    credit: 0.0,
+    debit: 15.75,
+    link: '/financial/overview/write-offs',
+  },
+  {
+    transferType: 'Invoices',
+    credit: 0.0,
+    debit: 120.0,
+    link: '/financial/invoice',
+  },
+]);
+
+onMounted(async () => {});
+</script>
+
+<style scoped lang="scss"></style>
--- apps/dashboard/src/modules/financial/views/overview/FinancialOverviewView.vue
+++ apps/dashboard/src/modules/financial/views/overview/FinancialOverviewView.vue
@@ -1,7 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <PageContainer>
+      <div class="flex flex-col gap-5 md:flex-col">
+        <Card class="w-full">
+          <template #title> {{ t('modules.user.financialOverview.title') }}</template>
+          <template #subtitle> {{ t('modules.user.financialOverview.subtitle') }} </template>
+          <template #content>
+            <Tabs v-model:value="year" class="w-full">
+              <TabList>
+                <Tab v-for="y in years" :key="y" :value="y.toString()">{{ y }}</Tab>
+              </TabList>
+            </Tabs>
+            <FinancialOverviewTable :sellers="sellers" :year="year" />
+          </template>
+        </Card>
+      </div>
+    </PageContainer>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FinancialOverviewTable from '@/modules/financial/views/overview/FinancialOverviewTable.vue';
+import { useI18n } from 'vue-i18n';
+import PageContainer from '@/layout/PageContainer.vue';
+import { onMounted, ref, type Ref } from 'vue';
+import { useFiscalYear } from '@/composables/fiscalYear.ts';
+import { useFinancialOverviewStore } from '@/stores/financial-overview.store.ts';
+
+const { t } = useI18n();
+
+const year: Ref<number> = ref(2026);
+const { getFiscalYearList, getFiscalYearRange } = useFiscalYear();
+const years = getFiscalYearList();
+const sellers = ref([]);
+
+const financialOverviewStore = useFinancialOverviewStore();
+onMounted(async () => {
+  await financialOverviewStore.fetchAllSellerUsers();
+  sellers.value = financialOverviewStore.sellers;
+});
+</script>
 
 <style scoped lang="scss"></style>
